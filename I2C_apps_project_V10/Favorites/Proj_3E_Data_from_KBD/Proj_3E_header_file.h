c80c93bf84daa0025f0ba66d44e0804b0882af3c
aclandmark <mark.osborne15@btinternet.com>
2020-04-24 20:04:10 +0100

    Create Proj_3E_header_file.h

---
+++ I2C_apps_project_V10/Favorites/Proj_3E_Data_from_KBD/Proj_3E_header_file.h
@@ -0,0 +1,19 @@
+
+
+#include "../../Resources_V12/Header_files/Basic_HW_setup.h"
+#include "../../Resources_V12/Subroutines/Basic_HW_plus_Timer.c"
+#include "../../Resources_V12/I2C_Subroutines/I2C_subroutines_1.c"
+#include "../../Resources_V12/I2C_Subroutines/I2C_subroutines_2.c"
+#include "../../Resources_V12/Subroutines/Basic_PC_comms.c"
+#include "../../Resources_V12/Subroutines/Arithmetic.c"
+#include "../../Resources_V12/Subroutines/Integers_to_from_PC.c"
+#include "../../Resources_V12/I2C_Subroutines/I2C_slave_Rx_Tx.c"
+
+
+#include "../../Resources_V12/Subroutines/Data_to_from_EEPROMs.c"
+#include "../../Resources_V12/Subroutines/Random_and_prime_nos.c"
+#include "../../Resources_V12/I2C_Subroutines/I2C_diagnostic.c"
+
+
+long Num_from_KBD_Local(char*);
+void display_in_scientific_notation (long, char*, char);
