ff379ad4734fb42430ad20244fb48b29ebe88511
aclandmark <mark.osborne15@btinternet.com>
2020-04-25 17:40:43 +0100

    Update Proj_9C_header_file.h

--- I2C_apps_project_V10/Proj_9_System_projects/Proj_9C_ATMEGA328_manual_cal/Proj_9C_header_file.h
+++ I2C_apps_project_V10/Proj_9_System_projects/Proj_9C_ATMEGA328_manual_cal/Proj_9C_header_file.h
@@ -5,7 +5,6 @@
 #include "../../Resources_V12/I2C_Subroutines/I2C_subroutines_1.c"
 #include "../../Resources_V12/I2C_Subroutines/I2C_subroutines_2.c"
 #include "../../Resources_V12/Subroutines/Basic_PC_comms.c"
-#include "../../Resources_V12/Header_files/I_O_from switches.h"	
 
 #include "../../Resources_V12/Subroutines/Integers_to_from_PC.c"
 
