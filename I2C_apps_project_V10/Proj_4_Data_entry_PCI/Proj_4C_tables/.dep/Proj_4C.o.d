ae7fc8a27bbeebc2331131dc26b90d87e78114a7
aclandmark <mark.osborne15@btinternet.com>
2020-04-26 21:52:17 +0100

    Create Proj_4C.o.d

---
+++ I2C_apps_project_V10/Proj_4_Data_entry_PCI/Proj_4C_tables/.dep/Proj_4C.o.d
@@ -0,0 +1,27 @@
+Proj_4C.o: Proj_4C.c Proj_4C_header_file.h \
+  ../../Resources_V12/Header_files/Basic_HW_setup.h \
+  ../../Resources_V12/Subroutines/Basic_HW_plus_Timer.c \
+  ../../Resources_V12/I2C_Subroutines/I2C_subroutines_1.c \
+  ../../Resources_V12/I2C_Subroutines/I2C_subroutines_2.c \
+  ../../Resources_V12/I2C_Subroutines/../../Resources_V12/Header_files/I_O_from\ switches.h \
+  ../../Resources_V12/Subroutines/Basic_PC_comms.c \
+  ../../Resources_V12/Header_files/I_O_from\ switches.h \
+  ../../Resources_V12/I2C_Subroutines/I2C_slave_Rx_Tx.c
+
+Proj_4C_header_file.h:
+
+../../Resources_V12/Header_files/Basic_HW_setup.h:
+
+../../Resources_V12/Subroutines/Basic_HW_plus_Timer.c:
+
+../../Resources_V12/I2C_Subroutines/I2C_subroutines_1.c:
+
+../../Resources_V12/I2C_Subroutines/I2C_subroutines_2.c:
+
+../../Resources_V12/I2C_Subroutines/../../Resources_V12/Header_files/I_O_from\ switches.h:
+
+../../Resources_V12/Subroutines/Basic_PC_comms.c:
+
+../../Resources_V12/Header_files/I_O_from\ switches.h:
+
+../../Resources_V12/I2C_Subroutines/I2C_slave_Rx_Tx.c:
