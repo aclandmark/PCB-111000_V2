36033a13f7c05f565b15a8da5ab1e58b8538826c
aclandmark <mark.osborne15@btinternet.com>
2020-04-26 12:59:36 +0100

    Update Proj_2A_header_file.h

--- I2C_apps_project_V10/Favorites/Proj_2A_reaction_time_tester_AVR/Proj_2A_header_file.h
+++ I2C_apps_project_V10/Favorites/Proj_2A_reaction_time_tester_AVR/Proj_2A_header_file.h
@@ -7,6 +7,7 @@
 #include "../../Resources_V12/Subroutines/Random_and_prime_nos.c"
 #include "../../Resources_V12/Subroutines/Basic_PC_comms.c"
 
-
+/*
 #include "../../Resources_V12/Subroutines/Data_to_from_EEPROMs.c"
 #include "../../Resources_V12/I2C_Subroutines/I2C_diagnostic.c"
+*/
