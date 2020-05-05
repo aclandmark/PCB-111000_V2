09404f926b229632d1c2fde168e4e651c302024d
aclandmark <mark.osborne15@btinternet.com>
2020-05-05 18:22:05 +0100

    Create Proj_3E.o.d

---
+++ Atmel_studio_7/Favorites/Proj_3E_Data_from_KBD/Proj_3E_Data_from_KBD/.dep/Proj_3E.o.d
@@ -0,0 +1,30 @@
+Proj_3E.o: Proj_3E.c Proj_3E_header_file.h \
+ ../../../Resources_V12/Header_files/Basic_HW_setup.h \
+ ../../../Resources_V12/Subroutines/Basic_HW_plus_Timer.c \
+ ../../../Resources_V12/I2C_Subroutines/I2C_subroutines_1.c \
+ ../../../Resources_V12/I2C_Subroutines/I2C_subroutines_2.c \
+ ../../../Resources_V12/I2C_Subroutines/../../Resources_V12/Header_files/I_O_from\ switches.h \
+ ../../../Resources_V12/Subroutines/Basic_PC_comms.c \
+ ../../../Resources_V12/Subroutines/Arithmetic.c \
+ ../../../Resources_V12/Subroutines/Integers_to_from_PC.c \
+ ../../../Resources_V12/I2C_Subroutines/I2C_slave_Rx_Tx.c
+
+Proj_3E_header_file.h:
+
+../../../Resources_V12/Header_files/Basic_HW_setup.h:
+
+../../../Resources_V12/Subroutines/Basic_HW_plus_Timer.c:
+
+../../../Resources_V12/I2C_Subroutines/I2C_subroutines_1.c:
+
+../../../Resources_V12/I2C_Subroutines/I2C_subroutines_2.c:
+
+../../../Resources_V12/I2C_Subroutines/../../Resources_V12/Header_files/I_O_from\ switches.h:
+
+../../../Resources_V12/Subroutines/Basic_PC_comms.c:
+
+../../../Resources_V12/Subroutines/Arithmetic.c:
+
+../../../Resources_V12/Subroutines/Integers_to_from_PC.c:
+
+../../../Resources_V12/I2C_Subroutines/I2C_slave_Rx_Tx.c:
