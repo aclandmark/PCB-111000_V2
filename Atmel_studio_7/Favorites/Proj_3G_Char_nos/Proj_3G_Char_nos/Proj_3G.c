fb1a3fa9b12ea41a321a7f38e853c55680a4dea8
aclandmark <mark.osborne15@btinternet.com>
2020-05-05 18:22:53 +0100

    Create Proj_3G.c

---
+++ Atmel_studio_7/Favorites/Proj_3G_Char_nos/Proj_3G_Char_nos/Proj_3G.c
@@ -0,0 +1,59 @@
+/*
+ * Proj_3G_Char_nos.c
+ *
+ * Created: 05/05/2020 09:01:50
+ * Author : Mark
+ */ 
+
+
+/*Switch location on PCB:	 SW3 - SW2 - SW1
+
+Note:
+This project has been compiled with Optimisation level 1 in an attempt to minimise occaisional unstable operation.*/
+
+
+
+#include "Proj_3G_header_file.h"
+
+
+
+
+char digit=1, op, mode;
+
+int main (void){
+
+op=0;
+mode = 'u';													//mode: signed or unsigned
+setup_HW;
+I2C_Tx_display_char(digit,mode);
+setup_and_enable_PCI;
+sei();
+
+
+while(1){
+switch(op){
+case 0: break;												//Static display
+case 1: I2C_Tx_display_char(++digit,mode);break;			//Increment "digit" before calling "I2C_Tx....."
+case 2: I2C_Tx_display_char(--digit, mode);break;}			//Decrement "digit" before calling "I2C_Tx....."
+
+Timer_T0_10mS_delay_x_m(15);}}
+
+
+
+/**************************************************************************************************/
+ISR(PCINT2_vect) {											//sw1 and sw3 interrupt
+if(switch_2_down)return;									//Ignore if sw2 is still down
+if((switch_1_up) && (switch_3_up)){op = 0; return;}		//Both switches up
+
+if(switch_1_down) {op = 1; return;}						//Digits increment
+if(switch_3_down) {op = 2; return;}}						//Digits decrement
+
+
+/**************************************************************************************************/
+ISR(PCINT0_vect){											//sw2 interrupt service routine
+if(switch_2_up)return;										//Ignore sw2 key release
+if((switch_1_down) || (switch_3_down))return;				//Ignore if sw1 or 3 are still down
+if (mode == 's')
+{mode = 'u'; I2C_Tx_display_char(digit,mode);return;}		//toggle display from signed to unsigned
+if (mode == 'u')
+{mode = 's'; I2C_Tx_display_char(digit,mode);return;}}		//and visa-versa
