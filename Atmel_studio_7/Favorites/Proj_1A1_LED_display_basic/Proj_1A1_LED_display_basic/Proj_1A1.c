db8b145c76cd2f4c1f471c1e0d7fada1d8189eb3
aclandmark <mark.osborne15@btinternet.com>
2020-05-06 16:05:46 +0100

    Update Proj_1A1.c

--- Atmel_studio_7/Favorites/Proj_1A1_LED_display_basic/Proj_1A1_LED_display_basic/Proj_1A1.c
+++ Atmel_studio_7/Favorites/Proj_1A1_LED_display_basic/Proj_1A1_LED_display_basic/Proj_1A1.c
@@ -1,14 +1,43 @@
+
 /*
- * Proj_1A1_LED_display_basic.c
- *
- * Created: 03/05/2020 20:44:53
- * Author : Mark
- */ 
+Proj_1A1_LED_display_basic.c
+
+A basic first program.
+
+It introduces the following project resources:
+		
+		Subroutines:	
+			I2C_Tx_2_integers(A,B)
+			This displays two numbers in binary form
+	 
+			Timer_T1_sub() 
+			This pauses program execution
+
+		Macro Set_up_HW which initialises
+			The watch dog timer
+			I2C slave
+			Switched inputs
+			The UART
+			Initialises default programs
+			Etc.
+		
+		Note: there is a simplified version 
+			Set_up_HW_basic
+			This omits the default programs
+
+
+Note: Click on Tools, Options, Environment, Fonts and Colours, 
+Scroll down Display items to VA brace matching and choose a 
+bright colour such as red.  This makes programs much easier to read.
+*/
+
+
+
+
+
+
 
 #include "Proj_1A1_header_file.h"
-
-
-
 
 int main (void){
 
