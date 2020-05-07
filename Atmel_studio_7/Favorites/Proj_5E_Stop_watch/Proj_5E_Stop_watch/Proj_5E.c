495979525ddfb7a9380958ba2ad57a8ffcca2155
aclandmark <mark.osborne15@btinternet.com>
2020-05-07 13:13:34 +0100

    Update Proj_5E.c

--- Atmel_studio_7/Favorites/Proj_5E_Stop_watch/Proj_5E_Stop_watch/Proj_5E.c
+++ Atmel_studio_7/Favorites/Proj_5E_Stop_watch/Proj_5E_Stop_watch/Proj_5E.c
@@ -1,9 +1,4 @@
-/*
- * Proj_5E_Stop_watch.c
- *
- * Created: 05/05/2020 12:45:31
- * Author : Mark
- */ 
+
 
 /*Proj_5E_Stop_watch
 *********************************************************/
@@ -31,7 +26,7 @@ USER INSTRUCTIONS
 
 
 
-Switch location SW3(PD2) - SW2(PB6) - SW1(PD7)*/
+Switch location SW1(PD2) - SW2(PB6) - SW3(PD7)*/
 
 
 
@@ -40,6 +35,7 @@ Switch location SW3(PD2) - SW2(PB6) - SW1(PD7)*/
 
 
 volatile char T1_ovf_flag = 0;										//requires volatile label
+
 																	//even though "T1_ovf_flag" is used in "main" 
 int main (void){
 
@@ -61,27 +57,32 @@ while(switch_1_down || switch_3_down);								//wait for switch release
 TIMSK1 |= (1 << TOIE1); sei();										//Enable timer 1 interrupt
 
 while(1){															//Infinite loop, requires sw2 interrupt to exit
-while(switch_1_up && switch_3_up);									//wait for a keypress
+	while(switch_1_up && switch_3_up);								//wait for a keypress
 
-if(switch_3_down)
-{I2C_Tx_Clock_command(store_time);									//save time and pause clock display
-while (switch_3_down);												//wait for key release
-I2C_Tx_Clock_command(display_current_time);}						//clock display continues as normal
+	if(switch_3_down)
+		{I2C_Tx_Clock_command(store_time);							//save time and pause clock display
+		while (switch_3_down);										//wait for key release
+		I2C_Tx_Clock_command(display_current_time);}				//clock display continues as normal
 
-Timer_T1_sub_with_interrupt(T1_delay_50ms);							//50mS pause: avoids switch bounce
-while(T1_ovf_flag == 0);T1_ovf_flag = 0;							//T1_ovf_flag is reset by T1 ISR after a 50ms delay
+	Timer_T1_sub_with_interrupt(T1_delay_50ms);						//50mS pause: avoids switch bounce
+	while(T1_ovf_flag == 0);T1_ovf_flag = 0;						//T1_ovf_flag is reset by T1 ISR after a 50ms delay
 
-if(switch_1_down)													//read stored times at 200ms intervals
-{I2C_Tx_Clock_command(display_stored_times);						//if sw1 is held down
-Timer_T0_10mS_delay_x_m(20);}		
-if(switch_1_down && switch_3_down)									//press sw3 while sw1` is still held down to
-{I2C_Tx_Clock_command(display_current_time); 						//continue clock display 
-while(switch_1_down || switch_3_down);}							//wait until both switches have been released
+	if(switch_1_down)												//read stored times at 200ms intervals
+		{I2C_Tx_Clock_command(display_stored_times);				//if sw1 is held down
+		Timer_T0_10mS_delay_x_m(20);}		
+	if(switch_1_down && switch_3_down)								//press sw3 while sw1` is still held down to
+		{I2C_Tx_Clock_command(display_current_time); 				//continue clock display 
+		while(switch_1_down || switch_3_down);}						//wait until both switches have been released
 }}				
 
 
-ISR(TIMER1_OVF_vect) {TCCR1B = 0; T1_ovf_flag = 1;}				//stop timer 1 and set overflow flag
 
+/*******************************************************************************************************************************/
+ISR(TIMER1_OVF_vect) {TCCR1B = 0; T1_ovf_flag = 1;}					//stop timer 1 and set overflow flag
+
+
+
+/*******************************************************************************************************************************/
 ISR(PCINT0_vect) {if(switch_2_up){return;}
 I2C_Tx_Clock_command(AT_exit_stop_watch);
 SW_reset;}
