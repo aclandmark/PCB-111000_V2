46c85f91c5fe8561dd1267b771309af8e768bb86
aclandmark <mark.osborne15@btinternet.com>
2023-08-29 17:17:29 +0100

    Update 1_Basic_Timer_IO_subs.c

--- Programmers/5_Project_pcb_168_V2.30_Arduino_V2/Resources/1_Basic_Timer_IO_subs.c
+++ Programmers/5_Project_pcb_168_V2.30_Arduino_V2/Resources/1_Basic_Timer_IO_subs.c
@@ -1,3 +1,9 @@
+
+
+void SBtoAL(char*, long, char);
+void NumericString_to_PC(char*);
+
+
 void newline(void){sendString ("\r\n");}
 void sendSpace(void){sendString ("  ");}
 
@@ -34,12 +40,12 @@ TCCR1B = 0;}
 
 
 /*******************************************************************************************/
-void delay_with_interrupt(char Counter_speed, unsigned char St_point){
+/*void delay_with_interrupt(char Counter_speed, unsigned char St_point){
 TIMSK0 |= (1 << TOIE0); 
 timer_T0_sub_with_interrupt(Counter_speed, St_point);
 while(!(T0_ovf_flag));T0_ovf_flag = 0;
 TIMSK0 &= (~(1 << TOIE0));}
-
+*/
 
 
 /*******************************************************************************************/
@@ -91,10 +97,6 @@ void sendHex(char radix, unsigned int Hex){ char print_out_string[6];
 my_utoa(radix, Hex, print_out_string, 'Z'); sendString_V(print_out_string);sendString(" ");}//include leading zeros
 
 
-/*******************************************************************************************/
-void sendCharasASKI(char radix, unsigned char CHR){ char print_out_string[4];
-my_chartoa(radix, CHR, print_out_string); sendString_V(print_out_string);sendString(" ");}
-
 
 /*******************************************************************************************/
 void USART_init (unsigned char UBRROH_N, unsigned char UBRR0L_N ){
@@ -124,90 +126,67 @@ c=s[i];  s[i] = s[j];  s[j] = c;}}
 
 
 
-/*******************************************************************************************/
-void my_chartoa(char radix, unsigned char n, char s[]){
-unsigned char i,sl,j,c;
-i=0; sl=0;
-do{sl++;
-s[i] = n % radix;
-if (s[i] < 10) s[i]+= '0';
-else s[i] += '7';
-i++;
-}while((n/=radix)>0);
-s[i] = '\0';
-for(i=0, j=sl-1; i<j; i++,j--){
-c=s[i];  s[i] = s[j];  s[j] = c;}}
+
+/*********************************************************************/
+void Num_to_PC(char radix, long int_num){
+char array[12];												//long  has 10 chars + sign + null terminator	
+SBtoAL(array, int_num, radix);								//calls the Binary to askii subroutine
+NumericString_to_PC(array);}	
 
 
 
-/*******************************************************************************************/
-int askiX2_to_hex (char a[]) {
-int hex;
-hex=0;
-if (non_numeric_char(a[0])) return 0;
-if (non_numeric_char(a[1])) return 0;
 
-a[0] = a[0] - '0'; if (a[0] >=17)a[0] = a[0]-7;
-hex = hex + (a[0]<<4);
-a[1] = a[1] - '0'; if (a[1] >=17)a[1] = a[1]-7;
-hex = hex + a[1];
-return hex;}
+/*********************************************************************/
+void SBtoAL(char array[], long num, char radix){					//Signed Binary to Askii
+int m=0;
+long sign;
+
+if (num == 0x80000000){
+switch(radix){
+case 10: array[0] = '8';array[1] = '4'; array[2] = '6';			//0x80000000 * -1 = 0
+array[3] = '3';array[4] = '8';array[5] = '4'; array[6] = '7';
+array[7] = '4';array[8] = '1';array[9] = '2';
+array[10] = '-'; array[11] = '\0'; break;
+
+case 16: array[0] = '0';array[1] = '0'; array[2] = '0';			//0x80000000 * -1 = 0
+array[3] = '0';array[4] = '0';array[5] = '0'; array[6] = '0';
+array[7] = '8';array[8] = '-';array[9] = '\0';
+array[10] = '\0'; array[11] = '\0'; break; } return;}
+
+for (int n = 0; n <=11; n++)array[n] = 0;	
+if ((sign = num) < 0) num = num * (-1);
+
+do {array[m] = num % radix;
+if(array[m] < 10)array[m] += '0'; else array[m] += '7';
+m++;} while ((num = num/radix) > 0);
+if (sign < 0) {array[m] = '-';m++;}}
 
 
 
-/*******************************************************************************************/
-char non_numeric_char (char data){
-if (((data > '9') && (data < 'A')) || (data < '0') || (data > 'F'))return 1;
-else return 0;}
 
 
-/*******************************************************************************************/
+/*********************************************************************/
+void NumericString_to_PC(char* s){					
+int n=0;
+while (s[n] != '\0')n++;									//scroll to end of string counting the number of characters
+for(int m = n; m; m--)sendChar(*(s + m-1));}	
+
+
+
+
 void sendsignedHex (int Hex){
 char print_out_string[6];
 char sign_char = '+';
 if (Hex & 0x8000) {Hex = ~Hex + 1; sign_char = '-';}
-my_utoa(10, Hex, print_out_string, 'N'); 		//No leading zeros
+my_utoa(10, Hex, print_out_string, 'N'); 												//Exclude leading zeros
 if (sign_char == '-')
-{sendChar('-'); sendString_V(print_out_string);sendString("  ");}
-else {sendChar(' '); sendString_V(print_out_string);sendString("  ");}}
+{sendChar('-'); NumericString_to_PC(print_out_string);sendString("  ");}
+else {sendChar(' '); NumericString_to_PC(print_out_string);sendString("  ");}}
 
 
 
-/*******************************************************************************************/
-char Rx_data(void){
-unsigned int n = 0;
-while (!(UCSR0A & (1 << RXC0))){n++;
-if (n > 15000) return 0;}			//was 15000 for BR of 600
-return UDR0;}
 
 
 
-/*******************************************************************************************/
-int askiX4_to_hex ( char a[])
-{int m, n;  int hex;
-hex=0;n=3;
-for(m=0; m<=3; m++){ 
-if (a[m]=='\0') {  hex = hex >> (4*(n+1)); return hex;}
-if(non_numeric_char(a[m])) return 0;
-a[m] = a[m] - '0'; if (a[m] >=17)a[m] = a[m]-7;
-hex = hex + (a[m] << (4*n)); n--;}
-return hex;}
 
-
-/*******************************************************************************************/
-int my_atoi_with_sign (char s[]){ int i,n;
-n=0;
-switch (s[0]){
-case '-' : 
-for(i=1; s[i] >= '0' && s[i] <= '9'; ++i)
-n= 10*n + (s[i] - '0'); n = 0-n; break;
-
-case '+' : 
-for(i=1; s[i] >= '0' && s[i] <= '9'; ++i)
-n= 10*n + (s[i] - '0'); break;
-
-default:
-for(i=0; s[i] >= '0' && s[i] <= '9'; ++i)
-n= 10*n + (s[i] - '0'); break;}
-return n;}
 
