7056ce702af1163f98d950eea8948a8587818d26
aclandmark <mark.osborne15@btinternet.com>
2023-09-13 09:43:39 +0100

    Update 1_AT_bootloader_V4_28_CC_SW_subs.c

--- Programmers/Mini-OS_V2_bootloaders/2_pcb_Bootloader_V4_28_CC/2_pcb_Bootloader_V4_28_CC/1_AT_bootloader_V4_28_CC_SW_subs.c
+++ Programmers/Mini-OS_V2_bootloaders/2_pcb_Bootloader_V4_28_CC/2_pcb_Bootloader_V4_28_CC/1_AT_bootloader_V4_28_CC_SW_subs.c
@@ -2,8 +2,8 @@
 void USART_init (unsigned char UBRROH_N, unsigned char UBRR0L_N ){
 {twenty_msec_delay;}	
 UCSR0B = 0;
-UBRR0H = UBRROH_N;  // for 9.6 and 38.4kB/s;
-UBRR0L = UBRR0L_N;  //25 for 38.4kHz baud rate for 8MHz osc. freq.
+UBRR0H = UBRROH_N; 
+UBRR0L = UBRR0L_N; 
 UCSR0A = (1 << U2X0);
 UCSR0B = (1 << RXEN0) | (1<< TXEN0);
 UCSR0C =  (1 << UCSZ00)| (1 << UCSZ01);} 
@@ -24,10 +24,6 @@ char receiveChar(void)
 {return UDR0;}
 
 
-
-/*void sendHex(char radix, unsigned int Hex){ char print_out_string[6];
-my_utoa(radix, Hex, print_out_string, 'Z'); sendString(print_out_string);sendString(" ");}//include leading zeros
-*/
 void sendChar(char data){
 while (!(UCSR0A & (1 << UDRE0)));
 UDR0 = data;}
@@ -35,32 +31,17 @@ UDR0 = data;}
 void waitforchar(void){unsigned char n=225; 																																			
 while (!(UCSR0A & (1 << RXC0))) {n--; if(n==0) break;}}
 
-/*
-void my_utoa(char radix, unsigned int n, char s[], char mode){
-unsigned char i,sl,j,c;
-i=0; sl=0;
-do{sl++;
-s[i] = n % radix;
-if (s[i] < 10) s[i]+= '0';
-else s[i] += '7';
-i++;
-}while((n/=radix)>0);
-if ((i < 4) && (mode == 'Z')){while(i < 4) {s[i++] = '0';sl++;}}	//new line
-s[i] = '\0';
-for(i=0, j=sl-1; i<j; i++,j--){
-c=s[i];  s[i] = s[j];  s[j] = c;}}*/
-
 void newline(void){sendString ("\r\n");}
 
 
 void new_record(void){int B_int_r_pointer;			
-next_char;				//while (r_pointer == w_pointer)waitforchar();  
+next_char; 
 B_int_r_pointer = r_pointer;
 line_length =  store[B_int_r_pointer]; inc_r_pointer; 
 Count_down = line_length/2; 
 if (line_length_old < 0x10)short_line=1;
 line_length_old = line_length;
-next_char;			//while (r_pointer == w_pointer)waitforchar(); 
+next_char; 
 B_int_r_pointer = r_pointer;
 Hex_address  =  (store[B_int_r_pointer]); inc_r_pointer; 
 Hex_address  = Hex_address/2;
@@ -78,16 +59,15 @@ else {LED_2_off;}
 
 void start_new_code_block(void){signed char page_offset;
 PIC_address = Hex_address;
-page_address = (Hex_address & PAmask);		//0x0FF0????
+page_address = (Hex_address & PAmask);
 write_address = Hex_address - page_address;
 page_offset = write_address;
 line_offset = Hex_address & 0x0007;
-offset = page_offset;	//	Hex_address & 0x0007;
+offset = page_offset;
 space_on_page = (PageSZ - page_offset);}
 
 
 void Program_record(void){
-//unsigned char Dummy_byte;
 while(Count_down){ 
 Count_down--;
 Flash_flag = 1;   
@@ -97,7 +77,6 @@ PIC_address++;
 space_on_page--;
 prog_counter++;       
 if (write_address == PageSZ){
-//Dummy_byte = Read_write_mem('W', page_address, 0x0);
 Read_write_mem('W', page_address, 0x0);
 Flash_flag = 0;
 write_address = 0;
@@ -112,15 +91,12 @@ Load_page('L', write_address, Hex_cmd & 0x00FF);
 Load_page('H', write_address, (Hex_cmd >> 8));}
 
 void decode_HEX_cmd_SUB(void){int A_int_r_pointer;
-///cli();	//UCSR0B &= (~(1<<RXCIE0));
-next_char;			//while (r_pointer == w_pointer)waitforchar();
+next_char;
 A_int_r_pointer = r_pointer;
-Hex_cmd = store[A_int_r_pointer];inc_r_pointer; //sei(); //UCSR0B |= (1<<RXCIE0);
+Hex_cmd = store[A_int_r_pointer];inc_r_pointer;
 }
 
 void write_page_SUB(signed int page_address){
-//char Dummy_byte;
-//Dummy_byte = Read_write_mem('W',  page_address, 0x0);
 Read_write_mem('W',  page_address, 0x0);
 Flash_flag = 0;}
 
