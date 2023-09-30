b03dee74a386f4708df93b273cb5e53dc88e18a0
aclandmark <mark.osborne15@btinternet.com>
2023-09-30 20:05:40 +0100

    Update I2C_subroutines_1.c

--- 9_Nano_Projects/Resources_nano_projects/Chip2chip_comms/I2C_subroutines_1.c
+++ 9_Nano_Projects/Resources_nano_projects/Chip2chip_comms/I2C_subroutines_1.c
@@ -1,4 +1,49 @@
 
+
+void I2C_Tx(char, char, char*);
+
+
+
+
+/************************************************************************/
+void I2C_Tx_2_integers(unsigned int s1, unsigned int s2){			
+char num_bytes=4; char mode = 1; char s[4];
+for (int m = 0;  m < 4; m++){
+switch (m){
+case 0: s[m] = s1; break; 											//Send S1 lower byte
+case 1: s[m] = s1 >> 8; break; 									//Send S1 higher byte									
+case 2: s[m] = s2; 	break;											//Send S1 lower byte
+case 3: s[m] = s2 >> 8; break;}}									//Send S1 higher byte
+I2C_Tx(num_bytes,mode, s);}
+
+
+
+
+
+
+/************************************************************************/
+void I2C_Tx_8_byte_array(char s[]){									//was I2C_Tx_1
+char num_bytes=8; char mode=4;
+I2C_Tx(num_bytes,mode, s);}
+
+
+
+
+
+/************************************************************************/
+void I2C_Tx_LED_dimmer(void){
+char Dimmer_control;
+
+if((PINB & 0x40)^0x40)Dimmer_control = 1;			//if Switch_2 down
+else Dimmer_control = 0;							//Normal operation
+
+I2C_Tx(1, 'Q', &Dimmer_control);}	
+
+
+
+
+
+
 /************************************************************************/
 void I2C_Rx_get_version_A(char str_type){			//NEEDED
 char num_bytes=1; char mode='P';
