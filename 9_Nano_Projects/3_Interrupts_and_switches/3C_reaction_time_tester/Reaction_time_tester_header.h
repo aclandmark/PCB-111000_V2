0ad87263579dd26fbd0938273aca13b07d9f0d3b
aclandmark <mark.osborne15@btinternet.com>
2023-10-02 19:05:49 +0100

    Update Reaction_time_tester_header.h

--- 9_Nano_Projects/3_Interrupts_and_switches/3C_reaction_time_tester/Reaction_time_tester_header.h
+++ 9_Nano_Projects/3_Interrupts_and_switches/3C_reaction_time_tester/Reaction_time_tester_header.h
@@ -191,10 +191,10 @@ Timer_T0_10mS_delay_x_m(16);
 /*****************************************************************************/
 #include "Resources_nano_projects/Subroutines/HW_timers.c"
 #include "Resources_nano_projects/PC_comms/Basic_Rx_Tx_Basic.c"
-#include "Resources_nano_projects/I2C_Subroutines/I2C_subroutines_1.c"
-#include "Resources_nano_projects/I2C_Subroutines/I2C_subroutines_2.c"
-#include "Resources_nano_projects/I2C_Subroutines/I2C_slave_Rx_Tx.c"
-#include "Resources_nano_projects/I2C_Subroutines/I2C_diagnostic.c"
+#include "Resources_nano_projects/Chip2chip_comms/I2C_subroutines_1.c"
+//#include "Resources_nano_projects/I2C_Subroutines/I2C_subroutines_2.c"
+#include "Resources_nano_projects/Chip2chip_comms/I2C_slave_Rx_Tx.c"
+//#include "Resources_nano_projects/I2C_Subroutines/I2C_diagnostic.c"
 #include "Resources_nano_projects/Subroutines/Random_and_prime_nos.c"
 
 
