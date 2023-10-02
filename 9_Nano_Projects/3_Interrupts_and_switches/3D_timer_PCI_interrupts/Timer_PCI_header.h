5aa51c8ae57ee99ec4158137fc8b96e5247880d6
aclandmark <mark.osborne15@btinternet.com>
2023-10-02 19:07:58 +0100

    Update Timer_PCI_header.h

--- 9_Nano_Projects/3_Interrupts_and_switches/3D_timer_PCI_interrupts/Timer_PCI_header.h
+++ 9_Nano_Projects/3_Interrupts_and_switches/3D_timer_PCI_interrupts/Timer_PCI_header.h
@@ -156,10 +156,10 @@ TWCR = (1 << TWINT);
 /*****************************************************************************/
 #include "Resources_nano_projects/Subroutines/HW_timers.c"
 #include "Resources_nano_projects/PC_comms/Basic_Rx_Tx_Basic.c"
-#include "Resources_nano_projects/I2C_Subroutines/I2C_subroutines_1.c"
-#include "Resources_nano_projects/I2C_Subroutines/I2C_subroutines_2.c"
-#include "Resources_nano_projects/I2C_Subroutines/I2C_slave_Rx_Tx.c"
-#include "Resources_nano_projects/I2C_Subroutines/I2C_diagnostic.c"
+#include "Resources_nano_projects/Chip2chip_comms/I2C_subroutines_1.c"
+//#include "Resources_nano_projects/I2C_Subroutines/I2C_subroutines_2.c"
+#include "Resources_nano_projects/Chip2chip_comms/I2C_slave_Rx_Tx.c"
+//#include "Resources_nano_projects/I2C_Subroutines/I2C_diagnostic.c"
 #include "Resources_nano_projects/Subroutines/Random_and_prime_nos.c"
 
 
