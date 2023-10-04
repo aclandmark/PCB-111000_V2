4d5d3d9c713ba74b52d52a0b15c4fd3e21e543ca
aclandmark <mark.osborne15@btinternet.com>
2023-10-04 17:10:13 +0100

    Update Segment_driver_header.h

--- 9_Nano_Projects/6_The_8_digit_display_and_IO/6B_Segment_driver/Segment_driver_header.h
+++ 9_Nano_Projects/6_The_8_digit_display_and_IO/6B_Segment_driver/Segment_driver_header.h
@@ -165,9 +165,8 @@ TWCR = (1 << TWINT);
 /*****************************************************************************/
 #include "Resources_nano_projects\Subroutines\HW_timers.c"
 #include "Resources_nano_projects\PC_comms\Basic_Rx_Tx_Arduino.c"
-#include "Resources_nano_projects\I2C_Subroutines\I2C_subroutines_1.c"
-#include "Resources_nano_projects\I2C_Subroutines\I2C_slave_Rx_Tx.c"
-#include "Resources_nano_projects\I2C_Subroutines\I2C_diagnostic_A.c"
+#include "Resources_nano_projects\Chip2chip_comms\I2C_subroutines_1.c"
+#include "Resources_nano_projects\Chip2chip_comms\I2C_slave_Rx_Tx.c"
 #include "Resources_nano_projects\Subroutines\Random_and_prime_nos.c"
 
 
