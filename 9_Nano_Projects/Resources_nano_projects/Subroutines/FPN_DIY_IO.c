d21fc255617fd1b712d9a20762b200f61604fc0d
aclandmark <mark.osborne15@btinternet.com>
2023-10-05 15:13:52 +0100

    Update FPN_DIY_IO.c

--- 9_Nano_Projects/Resources_nano_projects/Subroutines/FPN_DIY_IO.c
+++ 9_Nano_Projects/Resources_nano_projects/Subroutines/FPN_DIY_IO.c
@@ -34,7 +34,7 @@ float_display_mode = '0';\
 if (PCIenabled){disable_pci_on_sw2;disable_pci_on_sw1_and_sw3;}\
 while(1){\
 if(switch_2_down)float_display_mode = '1'; else float_display_mode = '0';\
-if(switch_3_down)float_display_mode = '2';\
+if(switch_1_down)float_display_mode = '2';\
 waiting_for_I2C_master;\
 send_byte_with_Nack(float_display_mode);\
 clear_I2C_interrupt;\
