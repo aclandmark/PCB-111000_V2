b44e480972737c61581e93cbeaa48c479fa2b43b
aclandmark <mark.osborne15@btinternet.com>
2023-10-19 16:08:01 +0100

    Update I2C_subroutines_2.c

--- 9_Nano_Projects/Resources_nano_projects/Chip2chip_comms/I2C_subroutines_2.c
+++ 9_Nano_Projects/Resources_nano_projects/Chip2chip_comms/I2C_subroutines_2.c
@@ -1,5 +1,34 @@
 
 
+
+
+/************************************************************************/
+void I2C_Tx_OS_timer(char mode, char s[]){								
+char num_bytes=8;
+I2C_Tx(num_bytes,mode, s);}
+
+/************************************************************************/
+void I2C_Tx_Clock_command(char timer_mode, char command){			
+char s[2]; char num_bytes=1;
+s[0] = command;
+I2C_Tx(num_bytes, timer_mode, s);}
+
+
+/************************************************************************/
+void I2C_initiate_10mS_ref(void){
+char num_bytes=0;
+char mode = 'F';
+
+waiting_for_I2C_master;
+send_byte_with_Ack(num_bytes);
+send_byte_with_Nack(mode);
+
+TWCR = (1 << TWINT) | (1 << TWEN);		//clear interrupt and leave I2C slave active
+TWCR |= (1 << TWEA) | (1 << TWIE);} 	//Slave mode with interrupt and Enable Acknowledge
+
+
+
+
 /************************************************************************/
 void PCB_test(void){					
 char num_bytes=0;		 
