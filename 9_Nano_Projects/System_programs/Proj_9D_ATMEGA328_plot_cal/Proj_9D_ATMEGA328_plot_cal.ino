2c2c4f5cffe2f611a0c736c4bdf894182528fbaf
aclandmark <mark.osborne15@btinternet.com>
2023-10-24 14:42:49 +0100

    Update Proj_9D_ATMEGA328_plot_cal.ino

--- 9_Nano_Projects/System_programs/Proj_9D_ATMEGA328_plot_cal/Proj_9D_ATMEGA328_plot_cal.ino
+++ 9_Nano_Projects/System_programs/Proj_9D_ATMEGA328_plot_cal/Proj_9D_ATMEGA328_plot_cal.ino
@@ -20,16 +20,19 @@ however T2 running under crystal control provides the 7.8125 mS interrupt.
 There is no auto_cal_project for the Atmega 328 because this runs automatically under the control 
 of the mini-OS following a POR.
 */
+//case 'M':  cal_plot_328(); break;              //Scans 328 cal-factor fronmm 0x10 to 0xF0
+//void cal_plot_328(void){              //Called by Proj_9F (mode M)
+//scan_328_cal_factors(void)          //0x10 to 0xF0;
 
 
 
-#include "Proj_9C_header_file.h"
+#include "Proj_9D_header_file.h"
 int main (void){
 
 
 
 long percentage_error;
-int Results[41], error;
+int Result, error;
 unsigned char osccal_MIN;
 unsigned char cal_UC, OSCCAL_DV;
 unsigned char  New_UC_value;  
@@ -42,6 +45,21 @@ Serial.write("\r\nATMEGA 328 manual calibration (please wait 10 seconds)\r\n\
 Cal factor user value   \t");
 
 
+scan_328_cal_factors();
+for(int m = 0x10;m <=0xf0; m++){
+Result = receive_byte_with_Ack();
+Result = (Result << 8) + receive_byte_with_Ack();
+Hex_to_PC_A(m,Num_string, '\t');
+Int_Num_to_PC_A(Result,Num_string, '\t');
+percentage_error = Result;
+Int_Num_to_PC_A(percentage_error*100/62500, Num_string, '%');
+newline_A();}
+clear_I2C_interrupt;
+
+
+
+
+/*
 I2C_Tx(1, 'N', &cal_mode);                                              //Initiate Atmega 328 calibration mode
 waiting_for_I2C_master;                                                 //Atmega 328 accepts request to calibrate             
 OSCCAL_DV = receive_byte_with_Ack();                                    //Receive 328 OSCCAL_default Value
@@ -78,9 +96,10 @@ else {newline_A();Serial.write('?');New_UC_value = Hex_from_KBD();Serial.write("
 waiting_for_I2C_master;                                               //Send OSCCAL_User_Value to AT328 device
 send_byte_with_Nack(New_UC_value);                                   //mini-OS will test that User cal is suitable
 clear_I2C_interrupt;
-
+*/
 /**************************************/
-waiting_for_I2C_master;
+
+/*waiting_for_I2C_master;
 Test_res =  receive_byte_with_Ack();
 
 
@@ -103,6 +122,8 @@ Hex_to_PC_A(New_UC_value, Num_string, '\t');
 New_UC_value = receive_byte_with_Nack();
 clear_I2C_interrupt;
 Hex_to_PC_A(New_UC_value, Num_string, '\r');}
+*/
+
 
 Serial.write("AK to repeat\r\n");
 waitforkeypress_A();SW_reset;}
