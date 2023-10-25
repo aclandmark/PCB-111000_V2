d885e8c18970e23beed04ca4b40dd53c7e123a7c
aclandmark <mark.osborne15@btinternet.com>
2023-10-25 18:16:33 +0100

    Update Proj_9D_ATMEGA328_plot_cal.ino

--- 9_Nano_Projects/System_programs/Proj_9D_ATMEGA328_plot_cal/Proj_9D_ATMEGA328_plot_cal.ino
+++ 9_Nano_Projects/System_programs/Proj_9D_ATMEGA328_plot_cal/Proj_9D_ATMEGA328_plot_cal.ino
@@ -20,30 +20,22 @@ however T2 running under crystal control provides the 7.8125 mS interrupt.
 There is no auto_cal_project for the Atmega 328 because this runs automatically under the control 
 of the mini-OS following a POR.
 */
-//case 'M':  cal_plot_328(); break;              //Scans 328 cal-factor fronmm 0x10 to 0xF0
-//void cal_plot_328(void){              //Called by Proj_9F (mode M)
-//scan_328_cal_factors(void)          //0x10 to 0xF0;
-
 
 
 #include "Proj_9D_header_file.h"
+
+
 int main (void){
 
 
 
 long percentage_error;
 long Result, error;
-unsigned char osccal_MIN;
-unsigned char cal_UC, OSCCAL_DV;
-unsigned char  New_UC_value;  
-char cal_mode=1, Test_res;
 char Num_string[12];
 
 setup_HW_Arduino_IO_Extra;
 
-Serial.write("\r\nATMEGA 328 manual calibration (please wait 10 seconds)\r\n\
-Cal factor user value   \r\n");
-
+Serial.write("\r\nATMEGA 328:  Calibration error plotted against OSCCAL\r\n");
 
 scan_328_cal_factors();
 for(int m = 0x10;m < 0xf0; m++){
@@ -54,7 +46,7 @@ Result = (Result << 8) + receive_byte_with_Ack();
 clear_I2C_interrupt;
 Hex_to_PC_A(m,Num_string, '\t');
 Int_Num_to_PC_A(m,Num_string, '\t');
-//Int_Num_to_PC_A(Result,Num_string, '\t');
+Int_Num_to_PC_A(Result,Num_string, '\t');
 percentage_error = Result;
 Int_Num_to_PC_A(percentage_error*100/62500, Num_string, '\t');//'%'
 newline_A();
@@ -66,79 +58,10 @@ Result = (Result << 8) + receive_byte_with_Nack();
 clear_I2C_interrupt;
 Hex_to_PC_A(0xf0,Num_string, '\t');
 Int_Num_to_PC_A(0xf0,Num_string, '\t');
-//Int_Num_to_PC_A(Result,Num_string, '\t');
+Int_Num_to_PC_A(Result,Num_string, '\t');
 percentage_error = Result;
-Int_Num_to_PC_A(percentage_error*100/62500, Num_string, '%');
+Int_Num_to_PC_A(percentage_error*100/62500, Num_string, '\t');//'%'
 newline_A();
-
-
-
-
-/*
-I2C_Tx(1, 'N', &cal_mode);                                              //Initiate Atmega 328 calibration mode
-waiting_for_I2C_master;                                                 //Atmega 328 accepts request to calibrate             
-OSCCAL_DV = receive_byte_with_Ack();                                    //Receive 328 OSCCAL_default Value
-osccal_MIN = receive_byte_with_Ack();                                   //Working value + 20  
-
-for(int m = 0; m <= 40; m++){                                           //Receive table giving 40 calibration error results
-Results[m] = receive_byte_with_Ack();
-Results[m] = (Results[m] << 8) + receive_byte_with_Ack();}  
-cal_UC = receive_byte_with_Nack();                                      //Receive 328 OSCCAL_User Value
-clear_I2C_interrupt;
-
-Hex_to_PC_A(cal_UC,Num_string, '\r');                                   //Print User cal factor
-
-Serial.write("Cal factor default value   \t");                          //Print OSCCAL_default_Value
-Hex_to_PC_A(OSCCAL_DV,Num_string, '\r');
-
-for(int m = 0; m <= 40; m++)                                            //Print table of values
-{Hex_to_PC_A(osccal_MIN,Num_string, '\t');
-Int_Num_to_PC_A(Results[m],Num_string, '\t');
-percentage_error = Results[m];
-Int_Num_to_PC_A(percentage_error*100/62500, Num_string, '%');
-osccal_MIN++;newline_A();}
-
-Serial.write("Enter new user cal value (Use capitol letters)\r\n\
-or enter FF to delete the user cal");                                   //Request new OSCCAL_User_Value
-
-New_UC_value = Hex_from_KBD();
-Serial.write ("\r\nPress y if OK or AOK and repeat");
-
-while(1){
-if(waitforkeypress_A() == 'y')break; 
-else {newline_A();Serial.write('?');New_UC_value = Hex_from_KBD();Serial.write(" y?");}}
-
-waiting_for_I2C_master;                                               //Send OSCCAL_User_Value to AT328 device
-send_byte_with_Nack(New_UC_value);                                   //mini-OS will test that User cal is suitable
-clear_I2C_interrupt;
-*/
-/**************************************/
-
-/*waiting_for_I2C_master;
-Test_res =  receive_byte_with_Ack();
-
-
-if(Test_res == 'X'){                                                //min-OS rejects user-cal value just entered
-error = receive_byte_with_Ack();
-error = (error << 8) + receive_byte_with_Nack();clear_I2C_interrupt;
-newline_A(); 
-//Num_to_PC(10, error); 
-Int_Num_to_PC_A(error,Num_string, ' ');
-Serial.write("  Error too great! Try again."); 
-newline_A();}
-
-if(Test_res == 'Y'){                                                //min-OS accepts user-cal value just entered
-Serial.write("\r\nValues saved to EEPROM   \t");
-
-New_UC_value = receive_byte_with_Ack();
-//Num_to_PC(16, New_UC_value); Serial.write("   \t");
-Hex_to_PC_A(New_UC_value, Num_string, '\t');
-
-New_UC_value = receive_byte_with_Nack();
-clear_I2C_interrupt;
-Hex_to_PC_A(New_UC_value, Num_string, '\r');}
-*/
-
 
 Serial.write("AK to repeat\r\n");
 waitforkeypress_A();SW_reset;}
