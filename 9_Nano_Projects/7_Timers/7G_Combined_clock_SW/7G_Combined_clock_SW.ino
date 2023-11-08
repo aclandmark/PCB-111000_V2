55b89ec1df96622f92ece77ea1e2f02a3ee5163f
aclandmark <mark.osborne15@btinternet.com>
2023-11-08 17:04:00 +0000

    Update 7G_Combined_clock_SW.ino

--- 9_Nano_Projects/7_Timers/7G_Combined_clock_SW/7G_Combined_clock_SW.ino
+++ 9_Nano_Projects/7_Timers/7G_Combined_clock_SW/7G_Combined_clock_SW.ino
@@ -108,6 +108,7 @@ else set_data_mode;
 if(data_mode_not_set)                               //Goes here following POR
 {User_prompt_A;
 //User_instructions;
+Serial.write(message_1);
 }
 
 for(int m = 0; m<=11; m++)
