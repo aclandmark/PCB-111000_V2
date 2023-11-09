6ec7fe8dd59520f4dba1f9a599c72824cbb99091
aclandmark <mark.osborne15@btinternet.com>
2023-11-09 16:47:16 +0000

    Update 7G_2_Combined_clock_SW.ino

--- 9_Nano_Projects/7_Timers/7G_2_Combined_clock_SW/7G_2_Combined_clock_SW.ino
+++ 9_Nano_Projects/7_Timers/7G_2_Combined_clock_SW/7G_2_Combined_clock_SW.ino
@@ -98,7 +98,7 @@ clear_Arduino_WDT_flag;                             //set it to to 0xFF
 setup_HW_for_clock_SW;
 
 Timer_T1_sub(T1_delay_100ms);
-if (switch_2_down)
+if ((switch_2_down) || (MCUSR & (1 << EXTRF)))
 {while(switch_2_down);clear_data_mode;                //Ensure that the user prompt is skipped
 Timer_T1_sub(T1_delay_100ms);}
 
