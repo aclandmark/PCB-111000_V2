59055d3956bca71ccb204c5fc1b71eafd3156ed0
aclandmark <mark.osborne15@btinternet.com>
2023-11-30 20:46:02 +0000

    Update 8A_reset_controller.ino

--- 9_Nano_Projects/8_Fourier/8A_reset_controller/8A_reset_controller.ino
+++ 9_Nano_Projects/8_Fourier/8A_reset_controller/8A_reset_controller.ino
@@ -1,62 +1,40 @@
 
-/*
- Program developed to check options for the reset function
-Two EEPROM locations are used:
-
-        MCUSR_copy      (0x3FC) This identifies POR and external reset and distinuishes beetween WDTouts due to the bootloader and user apps
-        reset_ctl_reg   (0x3ED) This distinguishes between user SW resets, WDTouts at which a flag is set and WDTouts which trigger an interrupt
-note:   All WDTouts trigger a jump to the bootloader (0x7000) except those which trigger an interrupt where the ISR is missing. These trigger a jump to 0x0000
- 
-Post reset the micro reads the EEPROM to determine its source.
- */
-
-
-
 #include "8A_header_file_1.h"
 
 int main (void){
 
 setup_HW;
-//One_25ms_WDT_with_interrupt;
-setup_watchdog;
- wdt_enable(WDTO_30MS);
-  while(switch_1_down)wdr();
 
+setup_watchdog;
+wdt_enable(WDTO_30MS);
+while((switch_1_down) || (switch_2_down) || (switch_3_down)) wdr();
+sei();
 switch (reset_status){
 
 case 1: User_prompt_Basic;
-Serial.write("\r\nProgram running following POR\t"); break;
-case 2: Serial.write("\r\nProgram triggered by a SW_reset.\t"); break;
-case 3: Serial.write("\r\nProgram triggered by external reset or run (the -r- keypress)."); break;
-case 4: Serial.write("\r\nProgram triggered by a flagged WDTout.\t"); break;
-case 5: Serial.write("\r\nProgram triggered by a WDTout with interrupt");break;
-case 6: Serial.write("\r\nJumps to 0x0000 rather than 0x7000\r\n(WDT configured with interrupt but ISR missing.)");break;}
+String_to_PC_Basic("\r\nProgram running following POR\t"); break;
+case 2: String_to_PC_Basic("\r\nProgram triggered by a SW_reset (sw1 pressed)\t"); break;
+case 3: String_to_PC_Basic("\r\nProgram triggered by pressing -r- at the p/r prompt (Ext reset)."); break;
+case 4: String_to_PC_Basic("\r\nProgram triggered by a SW_reset (sw3 pressed).\t"); break;
+case 5:String_to_PC_Basic("\r\nProgram triggered by a WDTout with interrupt (sw2 pressed)");break;}
+
+while(1){if((switch_1_up) && (switch_3_up))wdr(); else 
+
+if(switch_1_down)
+while(switch_1_down);
+
+if(switch_2_down){sixty_four_ms_WDT_with_interrupt;
+while(switch_2_down);}
+
+if(switch_3_down){eeprom_write_byte((uint8_t*)0x1FC, 0x0);
+while(switch_3_down);}}    
+
+return 1;}
 
 
-//set_up_PCI;
-//enable_pci_on_sw1;
-while(1){if(switch_1_up)wdr(); else while(switch_1_down);}                    //Waits here for the switches to be operated 
-return 1;
-}
-
-/*
-ISR(PCINT2_vect){ 
-if (switch_1_up)return;
-  sei();
-  disable_pci_on_sw1;
-if((switch_2_up) && (switch_3_up));                                             //Followed by SW_reset
-if((switch_2_down) && (switch_3_up)){Signal_flagged_WDTout;}                    //Followed by SW_reset
-if((switch_2_up) && (switch_3_down)){while(1);}                                 //WDT times out with interrupt                                 
-
-while(switch_1_down)wdr();                                                      //Wait for switch to be released
-
-SW_reset;}
+ISR (WDT_vect){eeprom_write_byte((uint8_t*)0x1FC, 0x01); }
 
 
-
-ISR (WDT_vect){Signal_WDTout_with_interrupt;wdt_enable(WDTO_15MS); }            //Optional
-
-*/
 
 
 /*******************************************************************************************************************/
