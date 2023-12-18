11e98bf47156e2a14c62b3446361cabe8ef117a4
aclandmark <mark.osborne15@btinternet.com>
2023-12-18 20:27:56 +0000

    Update 9E_e_power_series.ino

--- 9_Nano_Projects/9_FPN_routines/9E_e_power_series/9E_e_power_series.ino
+++ 9_Nano_Projects/9_FPN_routines/9E_e_power_series/9E_e_power_series.ino
@@ -8,8 +8,9 @@
  
 #include "e_power_series_header.h"
 
-#define message_1 "\r\nPower function: Enter +ve scientific number\r\n"
-#define message_2 "\r\n"
+#define message_1 "\r\nPower function: Enter +ve scientific number\r\n\
+Then Enter power.\r\n"
+#define message_2 "\r\nAccumulator reset.\r\n"
 #define Buff_Length  20
 
 #define Get_and_echo_Sc_num \
@@ -32,13 +33,19 @@ float Result;
 char sign;
 
 setup_HW_with_reset_analysis;
+wdt_enable(WDTO_30MS);
+while(switch_3_down)wdr();
 
-if(!(watch_dog_reset))Serial.write(message_1);
-if(watch_dog_reset){watch_dog_reset = 0; Serial.write(message_2);}
+if(reset_status == 3){ Serial.write(message_1);}
+else Serial.write(message_2);
 
 Serial.write("?  ");
+
 Get_and_echo_Sc_num;
 Num = SC_num;
+
+while(1){
+  
 Num_bkp = Num;
 
 if(FPN_GT_or_EQ(Num, 1.0))                                  //Multiply or divide number by 2 untill it
@@ -49,28 +56,35 @@ if (FPN_LT(Num, 1.0))
 {twos_exp = 0; while (FPN_LT(Num, 1.0))
 {Num = FPN_mult(Num, 2.0); twos_exp -= 1;}}
 
+logN = logE_power_series(Num); 
+if(twos_exp) logN 
++= FPN_mult((float)twos_exp, 0.6931472);                     //Log to base e of the scientific number
+
+
+
+if(reset_status == 3){
 FPN_to_String(Num, 1, 6, '\t',Num_string);
 Serial.write (Num_string);
 Int_Num_to_PC_A(twos_exp, Num_string, '\r');                  //Echo data to screen
 
-logN = logE_power_series(Num); 
-if(twos_exp) logN 
-+= FPN_mult((float)twos_exp, 0.6931472);                     //Log to base e of the scientific number
+
 
 Serial.write("Natural log is\t"); 
 FPN_to_String(logN, 1, 5, '\r',Num_string);
 Serial.write (Num_string);
 
-Serial.write("Enter power\t");
+Serial.write("Enter power\t");}
 Get_and_echo_Sc_num;
 Pow = SC_num;
+
+//while(switch_3_down);
 
 Serial.write("\r\nLocal result\t");
 Log_result = FPN_mult (logN, Pow);                          //The Log of the result
 
 if((FPN_LT (Log_result, 0.0005)) 
 && (FPN_GT (Log_result, -0.0005)))
-{Serial.write("Unity."); SW_reset;}                         //Result very close to one.
+{Serial.write("Unity.\r\n"); SW_reset;}                         //Result very close to one.
 
 Result = expE_power_series(Log_result);                     //Returns the antilog
 FPN_to_String(Result, 1, 5, '\r',Num_string);
@@ -82,6 +96,19 @@ Serial.write(Num_string);
 
 
 I2C_FPN_to_display(Result);//}
+display_FPN_short(Result, Num_string);
+while(switch_1_down)wdr();
+reset_status = 0;
+while(switch_3_down);
+
+Num = Result;
+
+Serial.write("New power?\t");
+
+
+
+}
+
 SW_reset;
 return 1;}
 
@@ -98,7 +125,7 @@ char sign = 0;
 
 if ((FPN_GT (Num, 85.0)) 
 || (FPN_LT (Num, -85.0)))
-{Serial.write("~Exceeds FPN bounds."); SW_reset;}
+{Serial.write("~Exceeds FPN bounds.\r\n"); SW_reset;}
 if (FPN_LT(Num, 0))
 {sign = 1; Num = FPN_mult (Num, (-1));}
 
@@ -107,7 +134,7 @@ term_counter = 1;
 ans = 1.0;
 ans_old = 1.0;
 
-while(1){
+while(1){wdr();
 term = FPN_mult(term, FPN_div(Num, (float)term_counter)); 
 term_counter += 1;
 
@@ -119,7 +146,7 @@ if((FPN_GT(FPN_div(difference, ans), -1e-5))
 
 ans_old = ans;}
 
-setup_watchdog;
+//setup_watchdog;
 
 if(!(sign))return ans;
 else return FPN_div(1.0, ans);}
@@ -142,14 +169,15 @@ else                                                    //Use power series to ca
 {
 int m = 1;
 term = 1.0;
-while(1){
+while(1){wdr();
 term = FPN_mult(term, FPN_div(Num, (float)m));
 
 if (m == 1){logE = term; difference= logE;}
 else{
   if (m%2)logE = FPN_add (logE, term);
   else logE = FPN_sub(logE, term); 
-  difference = FPN_sub(logE, logE_old); }
+  difference = FPN_sub(logE, logE_old);
+  }
 
 if((FPN_GT(FPN_div(difference, logE), -1e-5)) 
 && (FPN_LT(FPN_div(difference, logE), 1e-5)))break;
