9b1e9391659eedc21ff3e866e130abeef50e4e73
aclandmark <mark.osborne15@btinternet.com>
2023-12-22 19:51:47 +0000

    Update 9F_Trig_functions.ino

--- 9_Nano_Projects/9_FPN_routines/9F_Trig_functions/9F_Trig_functions.ino
+++ 9_Nano_Projects/9_FPN_routines/9F_Trig_functions/9F_Trig_functions.ino
@@ -9,7 +9,7 @@ https://proofwiki.org/wiki/Power_Series_Expansion_for_Real_Arcsine_Function
 
 #define Get_and_echo_Sc_num \
 SC_num = Scientific_number_from_KBD(Num_string, &sign, Buff_Length);\
-FPN_to_String(SC_num, 1, 5, '\t', Num_string);\
+FPN_to_String(SC_num, 2, 3, '=', Num_string);\
 Serial.write(Num_string);
 
 #define PIE 3.14159
@@ -21,38 +21,57 @@ int main (void)
 char Num_string[Buff_Length + 2];
                                  
 float Result;
-char sign;
+char sign, type;
 float Cosine;
 float Angle, SC_num;
 
 setup_HW_with_reset_analysis;
 wdt_enable(WDTO_30MS);
 while(switch_1_down)wdr();
-Serial.write("Angle in degrees?\t");
+Serial.write("Angle in degrees? = ");
 Get_and_echo_Sc_num;
 Angle = SC_num/57.2958;
-Serial.write("\r\nAngle in radians\t");
-FPN_to_String(Angle, 1, 5, '\r',Num_string);
+
+FPN_to_String(Angle, 1, 5, ' ',Num_string);
+Serial.write (Num_string);
+Serial.write(" Radians\t\t");
+Serial.write("s, c or t?\t");
+type = waitforkeypress_A();
+
+switch(type){
+  case 'c':Serial.write ("Cos = ");break;
+  case 's':Serial.write ("Sin = ");break;
+}
+
+
+Result = Sin_Cos(Angle, type);
+//Serial.write ("Cos = ");
+FPN_to_String(Result, 1, 5, '\t',Num_string);
 Serial.write (Num_string);
 
-Cosine = Sin_Cos(Angle, 'c');
-Serial.write ("Cosine:\t\t");
-FPN_to_String(Cosine, 1, 5, '\r',Num_string);
-Serial.write (Num_string);
-//while(1)wdr();
-I2C_FPN_to_display(Cosine);//}
-display_FPN_short(Cosine, Num_string);
-while(switch_1_down)wdr();
+display_FPN_short(Result, Num_string);
+while(switch_1_up)wdr();
 reset_status = 0;
-//while(switch_3_down);
-Serial.write("Arcos:\t\t");
-Angle = Arc_cos(Cosine, 'c');
-FPN_to_String(Angle, 1, 5, '\r',Num_string);
-Serial.write (Num_string);
-newline_A();
 
-I2C_FPN_to_display(Angle);//}
+//Serial.write("Arcos = ");
+switch(type){
+  case 'c':Serial.write ("Arccos = ");break;
+  case 's':Serial.write ("Arcsin = ");break;
+}
+
+
+Angle = Arc_cos(Result, type, 1);
 display_FPN_short(Angle, Num_string);
+FPN_to_String(Angle, 2, 3, ' ',Num_string);
+Serial.write (Num_string);
+Serial.write(" Degrees (");
+Angle = Arc_cos(Result, type, 2);
+FPN_to_String(Angle, 2, 3, ')',Num_string);
+Serial.write (Num_string);
+
+newline_A();newline_A();
+
+
 while(switch_1_down)wdr();
 
 
@@ -97,7 +116,7 @@ difference = Result;}
 return Result;}
 
 /******************************************************************************************************************/
-float Arc_cos(float Cos, char type){
+float Arc_cos(float Cos, char type, char lib){
 
 float Angle;
 float term;
@@ -105,6 +124,51 @@ float Q = 1.0;      //term counter
 float Cos_bkp;
 float difference;
 
+
+Cos_bkp = Cos;
+if (lib == 2){Angle = PIE/2.0 - Cos;
+Cos = Cos * Cos * Cos;}
+
+if (lib == 1){Angle = FPN_sub(FPN_div(PIE,2), Cos);
+Cos = FPN_mult(FPN_mult(Cos, Cos), Cos);}
+
+term = FPN_div(0.5,3.0);                 //term counter = 1
+
+Angle = FPN_sub (Angle, FPN_mult(term, Cos));
+difference = Angle;
+
+while(1){wdr();
+Q += 1.0;
+
+if (lib == 1){
+term = term * ((2.0*Q) - 1.0)*((2.0*Q) - 1.0)/(2.0*Q)/((2.0*Q)+1.0);
+Cos = Cos * Cos_bkp * Cos_bkp;}
+
+if (lib == 2){
+term = FPN_mult(term,FPN_mult(FPN_sub (FPN_mult(2.0,Q), 1.0), FPN_sub (FPN_mult(2.0,Q), 1.0)));
+term = FPN_div (term, (FPN_mult(FPN_mult(2.0,Q),FPN_add (FPN_mult(2.0,Q), 1.0))));
+Cos = FPN_mult(FPN_mult(Cos, Cos_bkp), Cos_bkp);}
+
+//if(lib == 2)Cos = FPN_mult(FPN_mult(Cos, Cos_bkp), Cos_bkp);
+//if(lib == 1)Cos = Cos * Cos_bkp * Cos_bkp;
+
+ 
+Angle = FPN_sub (Angle, FPN_mult(term, Cos));
+difference = FPN_sub(difference, Angle);
+if (FPN_LT(difference, 1e-6) && FPN_GT(difference, -1e-6))break;
+difference = Angle;}
+
+Angle = FPN_mult(Angle, 57.2958);
+
+if (type == 'c');
+if (type == 's'){Angle = Angle - 90.0; Angle = Angle * -1.0;}
+
+return Angle;
+
+
+
+if(lib == 3){
+  
 Cos_bkp = Cos;
 Angle = PIE/2.0 - Cos;
 Cos = Cos * Cos * Cos;
@@ -135,6 +199,9 @@ if (type == 'c');
 if (type == 's'){Angle = Angle - 90.0; Angle = Angle * -1.0;}
 
 return Angle;}
+
+
+}
 
 
 
