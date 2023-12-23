50599ab0502fa6497cc7316f4a8d1887d448660c
aclandmark <mark.osborne15@btinternet.com>
2023-12-23 17:32:19 +0000

    Update 9F_Trig_functions.ino

--- 9_Nano_Projects/9_FPN_routines/9F_Trig_functions/9F_Trig_functions.ino
+++ 9_Nano_Projects/9_FPN_routines/9F_Trig_functions/9F_Trig_functions.ino
@@ -21,7 +21,7 @@ int main (void)
 char Num_string[Buff_Length + 2];
                                  
 float Result;
-char sign, type;
+char sign, type, type_old;
 float Cosine;
 float Angle, SC_num;
 
@@ -41,10 +41,10 @@ type = waitforkeypress_A();
 switch(type){
   case 'c':Serial.write ("Cos = ");break;
   case 's':Serial.write ("Sin = ");break;
+  case 't':Serial.write ("Tan = ");break;
 }
-
-
-Result = Sin_Cos(Angle, type);
+if (type == 't')Result = Tan(Angle);
+else Result = Sin_Cos(Angle, type);
 //Serial.write ("Cos = ");
 FPN_to_String(Result, 1, 5, '\t',Num_string);
 Serial.write (Num_string);
@@ -53,21 +53,30 @@ display_FPN_short(Result, Num_string);
 while(switch_1_up)wdr();
 reset_status = 0;
 
-//Serial.write("Arcos = ");
 switch(type){
   case 'c':Serial.write ("Arccos = ");break;
   case 's':Serial.write ("Arcsin = ");break;
-}
+  case 't':Serial.write ("Arctan = ");break;}
 
-
+if (type == 't')Angle = Arc_Tan(Result);
+else 
 Angle = Arc_cos(Result, type, 1);
 display_FPN_short(Angle, Num_string);
 FPN_to_String(Angle, 2, 3, ' ',Num_string);
 Serial.write (Num_string);
-Serial.write(" Degrees (");
+Serial.write(" Degrees ");
+
+if (type == 't');
+else{Serial.write("(");
+
+//if ((type == 'c') && (Result > 0.75)){type_old= 'c';type = 's';}else type_old = 's';
+
 Angle = Arc_cos(Result, type, 2);
+
+//if ((type_old == 'c') && (Result > 0.75))Angle = 90.0 - Angle;
+
 FPN_to_String(Angle, 2, 3, ')',Num_string);
-Serial.write (Num_string);
+Serial.write (Num_string);}
 
 newline_A();newline_A();
 
@@ -76,9 +85,7 @@ while(switch_1_down)wdr();
 
 
 
-while(1);
-
-}
+while(1);}
 
 /**************************************************************************************************************************/
 float Sin_Cos(float Num, char type)
@@ -115,6 +122,14 @@ difference = Result;}
 
 return Result;}
 
+
+
+/******************************************************************************************************************/
+float Tan(float Angle){
+return Sin_Cos (Angle, 's')/Sin_Cos(Angle, 'c');}
+
+
+
 /******************************************************************************************************************/
 float Arc_cos(float Cos, char type, char lib){
 
@@ -123,7 +138,7 @@ float term;
 float Q = 1.0;      //term counter
 float Cos_bkp;
 float difference;
-
+int counter = 0;
 
 Cos_bkp = Cos;
 if (lib == 2){Angle = PIE/2.0 - Cos;
@@ -137,7 +152,7 @@ term = FPN_div(0.5,3.0);                 //term counter = 1
 Angle = FPN_sub (Angle, FPN_mult(term, Cos));
 difference = Angle;
 
-while(1){wdr();
+while(1){wdr(); 
 Q += 1.0;
 
 if (lib == 1){
@@ -147,50 +162,11 @@ Cos = Cos * Cos_bkp * Cos_bkp;}
 if (lib == 2){
 term = FPN_mult(term,FPN_mult(FPN_sub (FPN_mult(2.0,Q), 1.0), FPN_sub (FPN_mult(2.0,Q), 1.0)));
 term = FPN_div (term, (FPN_mult(FPN_mult(2.0,Q),FPN_add (FPN_mult(2.0,Q), 1.0))));
-Cos = FPN_mult(FPN_mult(Cos, Cos_bkp), Cos_bkp);}
-
-//if(lib == 2)Cos = FPN_mult(FPN_mult(Cos, Cos_bkp), Cos_bkp);
-//if(lib == 1)Cos = Cos * Cos_bkp * Cos_bkp;
-
+Cos = FPN_mult(FPN_mult(Cos, Cos_bkp), Cos_bkp);if(!((++counter)%10))Serial.write(".");}
  
 Angle = FPN_sub (Angle, FPN_mult(term, Cos));
 difference = FPN_sub(difference, Angle);
 if (FPN_LT(difference, 1e-6) && FPN_GT(difference, -1e-6))break;
-difference = Angle;}
-
-Angle = FPN_mult(Angle, 57.2958);
-
-if (type == 'c');
-if (type == 's'){Angle = Angle - 90.0; Angle = Angle * -1.0;}
-
-return Angle;
-
-
-
-if(lib == 3){
-  
-Cos_bkp = Cos;
-Angle = PIE/2.0 - Cos;
-Cos = Cos * Cos * Cos;
-
-term = FPN_div(0.5,3.0);                 //term counter = 1
-
-Angle = FPN_sub (Angle, FPN_mult(term, Cos));
-difference = Angle;
-
-while(1){wdr();
-Q += 1.0;
-term = term * ((2.0*Q) - 1.0)*((2.0*Q) - 1.0)/(2.0*Q)/((2.0*Q)+1.0);
-//term = FPN_mult(term,FPN_mult(FPN_sub (FPN_mult(2.0,Q), 1.0), FPN_sub (FPN_mult(2.0,Q), 1.0)));
-//term = FPN_div (term, (FPN_mult(FPN_mult(2.0,Q),FPN_add (FPN_mult(2.0,Q), 1.0))));
-
-Cos = Cos * Cos_bkp * Cos_bkp;
-//Cos = FPN_mult(FPN_mult(Cos, Cos_bkp), Cos_bkp);
- 
-Angle = FPN_sub (Angle, FPN_mult(term, Cos));
-difference = FPN_sub(difference, Angle);
-if (FPN_LT(difference, 1e-6) && FPN_GT(difference, -1e-6))break;
-//if ((difference <= 1E-6) && (difference >= -1E-6))break;
 difference = Angle;}
 
 Angle = FPN_mult(Angle, 57.2958);
@@ -201,7 +177,35 @@ if (type == 's'){Angle = Angle - 90.0; Angle = Angle * -1.0;}
 return Angle;}
 
 
+
+/*******************************************************************************************************************/
+float Arc_Tan(float Num){
+float term;
+float Result;
+
+if ((Num > -1.0) && (Num < 1.0)){
+term = Num;
+Result = Num;
+  for (int m = 3; m <50; m+=2){
+term = term *Num*Num/(float)m;
+  
+ if ((m+1)%4)Result = Result + term;
+ else Result = Result - term; 
+ term = term*(float)m; }}
+else{
+ term = 1.0/Num; 
+Result = term;
+for (int m = 3; m <20; m+=2){
+term = term/Num/Num/(float)m;
+if ((m+1)%4)Result = Result + term;
+ else   Result = Result - term; 
+ term = term *(float)m;}
+
+Result = PIE/2.0 - Result;
+if(Num <= 1.0) Result -= PIE;
 }
+
+return Result * 57.2958;}
 
 
 
