8dda818d549da2280b54c8becd8b0d8e67801674
aclandmark <mark.osborne15@btinternet.com>
2023-12-27 16:23:42 +0000

    Update 6B_Segment_driver.ino

--- 9_Nano_Projects/6_The_8_digit_display_and_IO/6B_Segment_driver/6B_Segment_driver.ino
+++ 9_Nano_Projects/6_The_8_digit_display_and_IO/6B_Segment_driver/6B_Segment_driver.ino
@@ -7,22 +7,27 @@
 #include "Segment_driver_header.h"
 
 
-#define message_1 "\r\nFor each digit press 0 to 7 then a to g for segments. Press x to reset display.\t"
-#define message_2 "Again!\t"
-
+#define message_1 "\r\nFor each digit press 0 to 7 then a to g for segments. Press x to reset display.\r\n"
+#define message_2 "\r\nAgain!"
+#define message_3 "\r\nWDTout with interrupt occurred\r\n\
+A wdr() statement is probably needed some where.\r\n"
 
 int main (void){
 
 char keypress = 0, digit_num=0;            
 
-setup_HW_Arduino_IO_Extra;
+setup_HW_Arduino;
+One_25ms_WDT_with_interrupt;
 
-if(!(watch_dog_reset))Serial.write(message_1);
-else Serial.write(message_2);{watch_dog_reset = 0;}
+switch(reset_status){
+  case POR_reset:             User_prompt_A;    Serial.write(message_1);break;
+  case WDT_reset:             Serial.write(message_2);break;
+  case External_reset:        Serial.write(message_1);break;
+  case WDT_with_ISR_reset:    Serial.write(message_3);_delay_ms(25);cli();setup_watchdog_A;while(1);break;}
 
 while(1){                                                               //Keep inputting data until x is received
 
-while(!(Serial.available()))wdr(); keypress = Serial.read();            
+while(!(Serial.available()))wdr(); keypress = Serial.read();   //        
   
 switch(keypress){                                                       //Test keypress
 case '0': case '1': case '2': 
@@ -34,7 +39,7 @@ case 'd': case 'e': case 'f':
 case 'g': I2C_Tx_any_segment(keypress, digit_num); break;
 
 case 'x': {                                                             //Press x to reset the display
-I2C_Tx_any_segment_clear_all();SW_reset;} break;  
+I2C_Tx_any_segment_clear_all();setup_watchdog_A; SW_reset;} break;  
 
 defaut: break;                                                            //if any other key (AOK) execute a SW_reset.
 
@@ -43,6 +48,6 @@ defaut: break;                                                            //if a
 }                                                                        //Repeat program
 
 
-
+ISR (WDT_vect){eeprom_write_byte((uint8_t*)0x1FC, 0x01); while(1);}
 
 /***************************************************************************************************************************/
