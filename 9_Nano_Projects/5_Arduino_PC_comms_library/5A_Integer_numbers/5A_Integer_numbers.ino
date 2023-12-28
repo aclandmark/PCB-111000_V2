559bca1c3378f0e5f410b4f48c04d952acd2cc77
aclandmark <mark.osborne15@btinternet.com>
2023-12-28 15:31:41 +0000

    Update 5A_Integer_numbers.ino

--- 9_Nano_Projects/5_Arduino_PC_comms_library/5A_Integer_numbers/5A_Integer_numbers.ino
+++ 9_Nano_Projects/5_Arduino_PC_comms_library/5A_Integer_numbers/5A_Integer_numbers.ino
@@ -28,19 +28,29 @@ Int_Num_from_PC_A() implements the backspace key press
 /************************************************************************************************************/
 #include "Integer_numbers_header.h"
 
+#define message_1 "\r\nEnter integer number:  PCB returns all factors of that number.(Press sw1 to cancel.)\r\n"
+#define message_2 "New number?\r\n"
+#define message_3 "\r\nCalculation cancelled. New number?\r\n"
+
+
 #define BL 30                     //Buffer length
 
 int main (void)  
   { 
     char num_string[BL + 2];
     long  num;
-    int m = 1;
+    long m = 1;
  
- setup_HW_Arduino_IO_Extra;
-
-  User_prompt_A;
+ setup_HW_Arduino;
+ set_up_pci_on_sw1_and_sw3;
+ enable_pci_on_sw1_and_sw3
  
-   Serial.write("\r\nInteger number");
+switch(reset_status){
+  case POR_reset:                 User_prompt_A;    Serial.write(message_1);break;
+  case WDT_reset:                 Serial.write(message_2);break;
+  case WDT_reset_with_flag:       Serial.write(message_3);break;
+  case External_reset:            Serial.write(message_1);break;}
+ 
 num = Int_Num_from_PC_A_Local(num_string,BL);
 
 newline_A();
@@ -49,7 +59,7 @@ Serial.write("\t");
 
 do{
 if(!(num%m)){Int_Num_to_PC_A_Local(m, num_string, '\t');}
-m += 1; wdr();}while(m < num);
+m += 1; if(!(m%10000))Serial.write(".");}while(m < num);
 
 SW_reset;
 return 1;  }
@@ -75,6 +85,11 @@ Serial.print(num_as_string);
 Serial.print(next_char);}
 
 
+ISR(PCINT2_vect){
+if(switch_3_down) return;
+if((switch_1_up) && (switch_3_up))return;
+ if(switch_1_down) eeprom_write_byte((uint8_t*)0x1FC, 0);
+ SW_reset;}
 
 
 /******************************************************************************************/
