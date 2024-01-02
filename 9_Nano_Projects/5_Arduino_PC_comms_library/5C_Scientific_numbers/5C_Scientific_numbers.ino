c0af80cbb1e82670557dea407c404a98a5085e2f
aclandmark <mark.osborne15@btinternet.com>
2024-01-02 15:12:23 +0000

    Update 5C_Scientific_numbers.ino

--- 9_Nano_Projects/5_Arduino_PC_comms_library/5C_Scientific_numbers/5C_Scientific_numbers.ino
+++ 9_Nano_Projects/5_Arduino_PC_comms_library/5C_Scientific_numbers/5C_Scientific_numbers.ino
@@ -4,7 +4,7 @@
 
 #define message_1 "\r\nThis program calculates roots iteratively.\r\n\
 Enter scientific number then integer root?\r\n"
-#define message_2 "\r\n\r\nEnter new data?\r\n"
+#define message_2 "\r\nEnter new data?\r\n"
 
 #define BL 30                                     //Buffer length
 
@@ -18,7 +18,7 @@ float Num, Num_bkp;
 float inc;
 int Tens_expt;
 float Tens_multiplier;
-float test;
+float Ten_power_root;
 
 char Num_string[BL + 2];
 
@@ -29,29 +29,32 @@ switch(reset_status){
   case WDT_reset:                 Serial.write(message_2);break;
  case External_reset:            Serial.write(message_1);break;}
 
-//Serial.write("Enter scientific number?\t");
+
 
 Num = Sc_Num_from_PC_A(Num_string, BL);           //User enters the scientific number
 Sc_Num_to_PC_A(Num, 1, 5, '\t');
 
-//Serial.write("Root (Integer)?\t");
 root = Sc_Num_from_PC_A(Num_string, BL); 
-Int_Num_to_PC_A(root, Num_string, '\r');
+Int_Num_to_PC_A(root, Num_string, '\t');
+
+Num_bkp = Num;
 
 
-test = 1.0;
-for(int p = 0; p < root; p++)test *= 10.0;
+/************************************Remove surplus ten's expontents*******************************************************************/
+Ten_power_root = 1.0;
+for(int p = 0; p < root; p++)Ten_power_root *= 10.0;
 Tens_expt = 0;
 Tens_multiplier = 1.0;
-Num_bkp = Num;
-if (Num_bkp > test){while(Num > test){Num /= test; Tens_expt += root;} 
+
+if (Num_bkp > Ten_power_root){while(Num > Ten_power_root){Num /= Ten_power_root; Tens_expt += root;} 
 for(int m = 0; m < Tens_expt; m += root)Tens_multiplier *= 10.0;}
 
-newline_A();
-Serial.print(Num,6); Serial.write('\t');Serial.print(Tens_expt);
-newline_A();newline_A();
+if (Num_bkp < 1.0)
+{while(Num < 1.0){Num *= Ten_power_root; Tens_expt += root;} 
+for(int m = 0; m < Tens_expt; m += root)Tens_multiplier /= 10.0;}
 
 
+/****************************************Root to best integer value. Start iteration at 2***************************************/
 inc = 1.0;
 product = 1.0;
 start_value = 2.0;
@@ -64,12 +67,10 @@ else break;
 start_value += inc;
 product = 1.0;}
 
-Serial.print(result,6); 
-Serial.write(" ");Serial.print(product_mem,6);
-
-if (product_mem > Num)result = 1.0;
+if (product_mem > Num)result = 1.0;                           //Initial value for numbers less than 4 
 
 
+/*************************Improve iteration starting with one decimal place then 2, 3......up to 6 in all******************************/
 for(int p = 1; p <= 6; p++)
 {inc /= 10.0;
 product = 1.0;
@@ -79,16 +80,17 @@ while(1){
 if (product <= Num){result = start_value; product_mem = product;}
 else break;
 start_value += inc;
-product = 1.0;}
+product = 1.0;}}
 
-Serial.write("\r\n");Serial.print(result,6); 
-Serial.write(" ");Serial.print(product_mem,5);
-_delay_ms(50);}
 
-result = result * Tens_multiplier;
-Serial.write("\r\n");Serial.write("\r\n");
+result = result * Tens_multiplier;                                  //Factor in the surplus exponent
 Sc_Num_to_PC_A(result, 2, 5, '\r');
 
 
-SW_reset;
-}
+SW_reset;}
+
+
+
+
+
+/****************************************************************************************************************************************/
