543cdd126ca2a581372a60e998831f63e905c13e
aclandmark <mark.osborne15@btinternet.com>
2024-01-19 15:28:50 +0000

    Create Proj_8A_askii_IO.c

---
+++ 9_Nano_Projects/8_Atmel_studio/Proj_8A_askii_IO/Proj_8A_askii_IO/Proj_8A_askii_IO.c
@@ -0,0 +1,18 @@
+/*
+ * Proj_8A_askii_IO.c
+ *
+ * Created: 19/01/2024 15:24:36
+ * Author : Mark
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
