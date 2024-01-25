07885290830026e72d525a269bc687fb48100926
aclandmark <mark.osborne15@btinternet.com>
2024-01-25 18:55:02 +0000

    Create Proj_8D_fp_arithmetic.c

---
+++ 9_Nano_Projects/8_Atmel_studio/Proj_8D_fp_arithmetic/Proj_8D_fp_arithmetic/Proj_8D_fp_arithmetic.c
@@ -0,0 +1,18 @@
+/*
+ * Proj_8D_fp_arithmetic.c
+ *
+ * Created: 25/01/2024 17:54:56
+ * Author : Mark
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
