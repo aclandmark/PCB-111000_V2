909567c95e615382bfa8a8650f617335c43afa99
aclandmark <mark.osborne15@btinternet.com>
2024-01-27 21:08:06 +0000

    Update Proj_8D_fp_arithmetic.c

--- 9_Nano_Projects/8_Atmel_studio/Proj_8D_fp_arithmetic/Proj_8D_fp_arithmetic/Proj_8D_fp_arithmetic.c
+++ 9_Nano_Projects/8_Atmel_studio/Proj_8D_fp_arithmetic/Proj_8D_fp_arithmetic/Proj_8D_fp_arithmetic.c
@@ -6,23 +6,49 @@
  */ 
 
 #define BL 20
+
 #include "fp_arithmetic_header.h"
 
 int main(void)
 {
-	float FPN_2;
+	float fpn_1, fpn_2, result;
 	char FPN_as_string[BL];
-	char keypress;
-	
+	//char keypress;
+	//char newline[4] = "\r\n";
+	char op;
+		
 	setup_HW;
-	String_to_PC_Basic("Enter FPN\r\n");
+	String_to_PC_Basic("Enter fpn (cr) then +,-,*,/,^,% or ~ then  fpn (cr)\r\n");
 	
 	while(1){
 		FPN_string_KBD_to_display(FPN_as_string);
-		sscanf (FPN_as_string, "%g", &FPN_2);
-		display_8_digit_num_string(FPN_as_string, FPN_2);
+		sscanf (FPN_as_string, "%g", &fpn_1);
+		display_8_digit_num_string(FPN_as_string, fpn_1);
+		op = waitforkeypress_Basic();
+		newline_Basic();
+		FPN_string_KBD_to_display(FPN_as_string);
+		sscanf (FPN_as_string, "%g", &fpn_2);
+		newline_Basic();
+		display_8_digit_num_string(FPN_as_string, fpn_2);
+		waitforkeypress_Basic();
 		
-		while(1){
+		switch(op){
+			case '+':result = fpn_1 + fpn_2; break;
+			case '-':result = fpn_1 - fpn_2; break;
+			case '*':result = fpn_1 * fpn_2;; break;
+			case '/':result = fpn_1 / fpn_2; break;
+			case '^':result = pow(fpn_1, fpn_2); break;
+			case '%':result = pow(fpn_1,(1/fpn_2)); break;
+			case '~':result = 1/fpn_1; break;
+		default: result = fpn_1+0; printf("!!?"); break;}
+		
+		sprintf(FPN_as_string, "%g    %c    %g  =  %g", fpn_1, op, fpn_2, result);
+		Num_String_to_PC(FPN_as_string);
+		display_8_digit_num_string(FPN_as_string, result);
+		
+		
+		
+		/*while(1){
 			keypress = waitforkeypress_Basic();
 			if (keypress == 'd'){FPN_2 /= 7.0; sprintf(FPN_as_string, "\t/7=\t%g", FPN_2);}
 			else if (keypress == 'm'){FPN_2 *= 7.0; sprintf(FPN_as_string, "\t*7=\t%g", FPN_2);}
@@ -32,6 +58,15 @@ int main(void)
 			sprintf(FPN_as_string, "\r\n%g", FPN_2);
 			Num_String_to_PC(FPN_as_string);
 			
-		display_8_digit_num_string(FPN_as_string, FPN_2);}
-	String_to_PC_Basic("\r\nNew_FPN?\r\n");}}
+		display_8_digit_num_string(FPN_as_string, FPN_2);}*/
+		
+		
+		
+		
+	String_to_PC_Basic("\r\nNew_FPN?\r\n");}
+	
+	
+	
+	
+	}
 
