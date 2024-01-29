7d7e9e98dd6fe10603b8efad4fcc81da4f78a930
aclandmark <mark.osborne15@btinternet.com>
2024-01-29 20:22:00 +0000

    Update Proj_8D_fp_arithmetic.c

--- 9_Nano_Projects/8_Atmel_studio/Proj_8D_fp_arithmetic/Proj_8D_fp_arithmetic/Proj_8D_fp_arithmetic.c
+++ 9_Nano_Projects/8_Atmel_studio/Proj_8D_fp_arithmetic/Proj_8D_fp_arithmetic/Proj_8D_fp_arithmetic.c
@@ -1,36 +1,31 @@
-/*
- * Proj_8D_fp_arithmetic.c
- *
- * Created: 25/01/2024 17:54:56
- * Author : Mark
- */ 
+ 
 
-#define BL 20
+#define BL 30
 
 #include "fp_arithmetic_header.h"
 
 int main(void)
 {
-	float fpn_1, fpn_2, result;
+	float fpn_1, fpn_2, result=1.0;
 	char FPN_as_string[BL];
-	//char keypress;
-	//char newline[4] = "\r\n";
 	char op;
 		
 	setup_HW;
-	String_to_PC_Basic("Enter fpn (cr) then +,-,*,/,^,% or ~ then  fpn (cr)\r\n");
+	String_to_PC_Basic("\r\nEnter fpn (cr) then +,-,*,/,^,% or ~ then  fpn (cr)\r\n");
+	
+	FPN_string_KBD_to_display(FPN_as_string);
+	sscanf (FPN_as_string, "%g", &fpn_1);
+	display_8_digit_num_string(FPN_as_string, fpn_1);
+	
+	op = waitforkeypress_Basic(); Char_to_PC_Basic(op);
 	
 	while(1){
-		FPN_string_KBD_to_display(FPN_as_string);
-		sscanf (FPN_as_string, "%g", &fpn_1);
-		display_8_digit_num_string(FPN_as_string, fpn_1);
-		op = waitforkeypress_Basic();
-		newline_Basic();
-		FPN_string_KBD_to_display(FPN_as_string);
-		sscanf (FPN_as_string, "%g", &fpn_2);
-		newline_Basic();
-		display_8_digit_num_string(FPN_as_string, fpn_2);
-		waitforkeypress_Basic();
+		if (!(arithmetic_op(op))) {op = '?'; result = fpn_1;}
+		if((op != '~') && (op != '?'))
+		{FPN_string_KBD_to_display(FPN_as_string);
+			sscanf (FPN_as_string, "%g", &fpn_2);
+			newline_Basic();
+			display_8_digit_num_string(FPN_as_string, fpn_2);}
 		
 		switch(op){
 			case '+':result = fpn_1 + fpn_2; break;
@@ -39,34 +34,23 @@ int main(void)
 			case '/':result = fpn_1 / fpn_2; break;
 			case '^':result = pow(fpn_1, fpn_2); break;
 			case '%':result = pow(fpn_1,(1/fpn_2)); break;
-			case '~':result = 1/fpn_1; break;
-		default: result = fpn_1+0; printf("!!?"); break;}
-		
-		sprintf(FPN_as_string, "%g    %c    %g  =  %g", fpn_1, op, fpn_2, result);
+			case '~':result = 1/fpn_1; break;}
+					
+		if (op == '?')sprintf(FPN_as_string, "\r\n%g    %c", fpn_1, op);
+		else if(op != '~')sprintf(FPN_as_string, "%g    %c    %g  =  %g", fpn_1, op, fpn_2, result);
+		else sprintf(FPN_as_string, "\r\n1/%g  =  %g", fpn_1, result);
 		Num_String_to_PC(FPN_as_string);
+		Timer_T0_10mS_delay_x_m(50);
 		display_8_digit_num_string(FPN_as_string, result);
 		
-		
-		
-		/*while(1){
-			keypress = waitforkeypress_Basic();
-			if (keypress == 'd'){FPN_2 /= 7.0; sprintf(FPN_as_string, "\t/7=\t%g", FPN_2);}
-			else if (keypress == 'm'){FPN_2 *= 7.0; sprintf(FPN_as_string, "\t*7=\t%g", FPN_2);}
-			else break;
-			
-			Num_String_to_PC(FPN_as_string);
-			sprintf(FPN_as_string, "\r\n%g", FPN_2);
-			Num_String_to_PC(FPN_as_string);
-			
-		display_8_digit_num_string(FPN_as_string, FPN_2);}*/
-		
-		
-		
-		
-	String_to_PC_Basic("\r\nNew_FPN?\r\n");}
+		fpn_1 = result;
+	op = waitforkeypress_Basic(); Char_to_PC_Basic(op);
+	if(op == 'x')SW_reset;}}
 	
 	
-	
-	
-	}
 
+char arithmetic_op(char op){
+switch (op)
+{case '+': case '-': case '*': case '/':	
+case '^': case '%': case '~': return 1;break;}
+return 0;}
