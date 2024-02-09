66a6a36e828190c046b422fce477f66a1bfe50f3
aclandmark <mark.osborne15@btinternet.com>
2024-02-09 18:13:51 +0000

    Update 9C_Addition_Subtraction.ino

--- 9_Nano_Projects/9_FPN_routines/9C_Addition_Subtraction/9C_Addition_Subtraction.ino
+++ 9_Nano_Projects/9_FPN_routines/9C_Addition_Subtraction/9C_Addition_Subtraction.ino
@@ -1,4 +1,8 @@
-/*******************************************/
+/*******************************************
+  Addition and subtraction.
+  The two numbers are adjusted so that they have the same exponent
+ 
+ ********************************************/
 
 #include "9C_header.h"
 
@@ -27,6 +31,7 @@ Sc_Num_to_PC_A(FPN_1, 1, 6, ' '); Serial.write(" + ");
 
 while(1){
  FPN_2 = Scientific_number_from_KBD(num_as_string, &sign, Buff_Length);
+ if (FPN_2 > 0)Serial.write('+');
 Sc_Num_to_PC_A(FPN_2, 1, 6, ' '); Serial.write("= "); 
 
 Result = FPN_add_Local(FPN_1, FPN_2);
@@ -35,7 +40,6 @@ Serial.write(num_as_string);
 
 if ((*(long *)&Result)){I2C_FPN_to_display(Result); Serial.write("? ");}
 else {I2C_Tx_long(0);while(switch_3_up)wdr(); while(switch_3_down);}
-//display_FPN_short(Result, num_as_string);
 while(switch_1_down)wdr();
 FPN_1 = Result;
 while(switch_3_down);}
@@ -63,22 +67,26 @@ if (sign_2 == '-')FPN_digits_2 = -FPN_digits_2;
 
 twos_expnt_3 = twos_expnt_1;
 
-if (twos_expnt_1 > twos_expnt_2){
-  while(twos_expnt_1 > twos_expnt_2){twos_expnt_2 += 1; FPN_digits_2 = FPN_digits_2 >> 1;}twos_expnt_3 = twos_expnt_2;}
+if (twos_expnt_1 > twos_expnt_2){                                         //Divide FPN_digits_2 by 2 and increment twos_expnt_2 
+  while(twos_expnt_1 > twos_expnt_2){twos_expnt_2 += 1;                   //untill twos_expnt_1 is equal to twos_expnt_2
+  FPN_digits_2 = FPN_digits_2 >> 1;}twos_expnt_3 = twos_expnt_2;}         //Set twos_expnt_3 = twos_expnt_2 
 
-if (twos_expnt_1 < twos_expnt_2){
-  while(twos_expnt_1 < twos_expnt_2){twos_expnt_1 += 1; FPN_digits_1 = FPN_digits_1 >> 1;}twos_expnt_3 = twos_expnt_1;}
+if (twos_expnt_1 < twos_expnt_2){                                         //Repeat for twos_expnt_2 > twos_expnt_1
+  while(twos_expnt_1 < twos_expnt_2){twos_expnt_1 += 1; 
+  FPN_digits_1 = FPN_digits_1 >> 1;}twos_expnt_3 = twos_expnt_1;}
 
-FPN_digits_3 = FPN_digits_1 + FPN_digits_2;
+FPN_digits_3 = FPN_digits_1 + FPN_digits_2;                                 //Signs are included in this sum
 
-if (sign_1 != sign_2)
+if (sign_1 != sign_2)                                                       //Subtraction
 {
-    if (!(FPN_digits_3)){*(long*)(&Result) = 0x0; return Result;}
+    if (!(FPN_digits_3)){*(long*)(&Result) = 0x0; return Result;}           //zero result
   
- if (FPN_digits_3 & (unsigned long) 0x80000000){FPN_digits_3 *= -1; sign_3 = '-';}
-while (!(FPN_digits_3 & (0x40000000))){FPN_digits_3 = FPN_digits_3 << 1; twos_expnt_3 -= 1;}}
+ if (FPN_digits_3 & (unsigned long) 0x80000000)                              //negative result
+ {FPN_digits_3 *= -1; sign_3 = '-';}                                         //Extract sign
+while (!(FPN_digits_3 & (0x40000000)))                                       //Shift MSB to bit 30 and adjust exponent
+{FPN_digits_3 = FPN_digits_3 << 1; twos_expnt_3 -= 1;}}
 
-if(sign_1 == sign_2){
+if(sign_1 == sign_2){                                                        //Both +ve or both -ve
 
 if(twos_expnt_3 == 128){Serial.write("Infinity");SW_reset;}
 
@@ -86,15 +94,15 @@ switch(sign_1){
 case '+': break;
 
 case '-': 
-FPN_digits_3 =    FPN_digits_1 * -1 + FPN_digits_2 * -1; ; 
+FPN_digits_3 =    FPN_digits_1 * -1 + FPN_digits_2 * -1;                      //Both -ve: Repeat sum and set sign of result to -ve 
 sign_3 = '-';break;}
 
-if (FPN_digits_3 & (unsigned long) 0x80000000)
+if (FPN_digits_3 & (unsigned long) 0x80000000)                                //Overflow: shift right to vacate the sign bit (31)
 {FPN_digits_3 = (unsigned long)FPN_digits_3  >> 1; twos_expnt_3 += 1;}}
 
-Result = Assemble_FPN((unsigned long) FPN_digits_3, twos_expnt_3, sign_3);
+Result = Assemble_FPN((unsigned long) FPN_digits_3, twos_expnt_3, sign_3);    //Assemble floating point number
 if (sign_3 == '-')
-*(long*)&Result = *(long*)&Result | (unsigned long)0x80000000; 
+*(long*)&Result = *(long*)&Result | (unsigned long)0x80000000;                  //re-instate the sign bit
 return Result;}
 
 
