14b1eb3b6188d95ff0762760bea6f7828f3e098f
aclandmark <mark.osborne15@btinternet.com>
2024-03-02 14:51:29 +0000

    Update Mux_demo.ino

--- 9_Nano_Projects/10_Odds_and_ends/Mux_demo/Mux_demo.ino
+++ 9_Nano_Projects/10_Odds_and_ends/Mux_demo/Mux_demo.ino
@@ -6,7 +6,8 @@
 int main (void)
 { setup_HW;
 char Mux_cntl = 0;
- 
+
+ String_to_PC_Basic("\r\nPulse sw1 to demonstrate multiplexer operation\r\n");
 while(1){
 I2C_Tx(1, 'L', &Mux_cntl);
 while(switch_1_up);
