b6241c945e74c2140a59d17b9e3d128f9dbceeae
aclandmark <mark.osborne15@btinternet.com>
2024-03-06 17:01:34 +0000

    Create Mini-OS_V30_CC.c

---
+++ Mini-OS_V2_with_IC/Mini-OS_V30_CC/Mini-OS_V30_CC/Mini-OS_V30_CC.c
@@ -0,0 +1,18 @@
+/*
+ * Mini-OS_V30_CC.c
+ *
+ * Created: 06/03/2024 16:58:18
+ * Author : Mark
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
