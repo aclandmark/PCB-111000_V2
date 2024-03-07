1fe2774953715fdce40d3d458565f3a768feca02
aclandmark <mark.osborne15@btinternet.com>
2024-03-07 15:52:35 +0000

    Update Mini-OS_V30_CC.c

--- Mini-OS_V2_with_IC/Mini-OS_V30_CC/Mini-OS_V30_CC/Mini-OS_V30_CC.c
+++ Mini-OS_V2_with_IC/Mini-OS_V30_CC/Mini-OS_V30_CC/Mini-OS_V30_CC.c
@@ -52,7 +52,7 @@ int main (void){
 	Projects V1_9 and Bootloader V4_21_CA.\
 	External programmer V2_30B\r\n";*/
 	
-	char *SW_Version = "System files: Mini-OS_V26_0_CC plus pcb_Bootloader_V4_28_CC \r\n";
+	char *SW_Version = "System files: Mini-OS_V30_CC plus pcb_Bootloader_V4_28_CC \r\n";
 	
 	
 	char *SW_info = "SW information:\
