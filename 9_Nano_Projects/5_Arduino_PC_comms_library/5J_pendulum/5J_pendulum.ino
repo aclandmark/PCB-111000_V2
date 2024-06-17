09801da58372bf885f8b5edf7b1c9dab3920ff80
aclandmark <mark.osborne15@btinternet.com>
2024-06-17 17:48:41 +0100

    Update 5J_pendulum.ino

--- 9_Nano_Projects/5_Arduino_PC_comms_library/5J_pendulum/5J_pendulum.ino
+++ 9_Nano_Projects/5_Arduino_PC_comms_library/5J_pendulum/5J_pendulum.ino
@@ -2,10 +2,16 @@
 #include "pendulum.h"
  int rate;
 
- int main (void)                          //Example 1
+ int main (void)
   {
     char mode;
      setup_HW_Arduino;
+     
+     Thirty_two_ms_WDT_with_interrupt;
+
+  set_up_pci_on_sw2;
+  enable_pci_on_sw2;
+  
 rate = 20;
 
 while(1){
@@ -40,6 +46,7 @@ initialise_timer(m);
    PORT_1 = PORT_1 << 1; }}
 
 
+
 /**********************************************************************************************/
 void ascending (int num_steps, char mode){
       unsigned int count, top, Start_point;
@@ -66,6 +73,7 @@ else I2C_Tx_2_integers(0, PORT_1);
    PORT_1 = PORT_1 >> 1; }}
 
 
+
 /**********************************************************************************************/
 float initialise_timer(int m){
    unsigned int count, top, Start_point;
@@ -82,6 +90,26 @@ count = (unsigned int)(Time * 65000.0)/rate;
     Timer_T1_sub(4, Start_point);}
 
 
+
+/**********************************************************************************************/
+ISR(PCINT0_vect)
+{ if (switch_2_up)return; 
+  sei();
+  while(1);}
+
+
+
+/**********************************************************************************************/
+ISR (WDT_vect){eeprom_write_byte((uint8_t*)0x1FA, 0x01);
+
+eeprom_write_byte((uint8_t*)0x1F8, rate);
+Reset_Atmega328;
+Reset_I2C;while(1);}
+  
+
+
+
+/***************************************************************************************************************************/
 
 
    /**************************************************************************************/
