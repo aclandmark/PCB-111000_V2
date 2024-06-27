0cd2797fd9211057a4b2252c0e4ec201b78f2d7e
aclandmark <mark.osborne15@btinternet.com>
2024-06-27 15:15:41 +0100

    Update 6G_INT_from_IO.ino

--- 9_Nano_Projects/6_The_8_digit_display_and_IO/6G_INT_from_IO/6G_INT_from_IO.ino
+++ 9_Nano_Projects/6_The_8_digit_display_and_IO/6G_INT_from_IO/6G_INT_from_IO.ino
@@ -7,7 +7,12 @@
  * 
 */
 
-
+/*
+EEPROM usage
+0x1FF, 0x1FE and 0x1FD     OSCCAL
+0x1FC and 0x1FB            PRN generator
+0x1FA                      Reset source
+*/
 
 #include "INT_IO_to_display_header.h"
 
@@ -116,7 +121,7 @@ Data_Entry_complete=1;}                                           //Return to Li
 
 
 /****************************************************************************************************************/
-ISR (WDT_vect){eeprom_write_byte((uint8_t*)0x1FC, 0x01); while(1);}
+ISR (WDT_vect){eeprom_write_byte((uint8_t*)0x1FA, 0x01); while(1);}
 
 
 
