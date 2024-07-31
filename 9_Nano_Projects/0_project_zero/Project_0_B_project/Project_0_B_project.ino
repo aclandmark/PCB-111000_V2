3ba76b598319e851cf102b899891b206aecd815a
aclandmark <mark.osborne15@btinternet.com>
2024-07-31 18:31:30 +0100

    Create Project_0_B_project.ino

---
+++ 9_Nano_Projects/0_project_zero/Project_0_B_project/Project_0_B_project.ino
@@ -0,0 +1,22 @@
+/*
+EEPROM usage
+0x1FF, 0x1FE and 0x1FD    OSCCAL
+0x1FC and 0x1FB       PRN generator
+*/
+
+
+#include "Project_0_header.h"
+
+
+ int main (void)                          //Example 1
+  { 
+
+  setup_HW;
+
+while(1){
+
+while (switch_1_up);{switch_LED_1;}
+Timer_T0_10mS_delay_x_m(20);          //Alternative line
+}
+  
+    }
