4273e8572787385213f609e1a691f4fbbf49c93b
aclandmark <mark.osborne15@btinternet.com>
2024-08-05 18:32:49 +0100

    Create Project_0_E_project.ino

---
+++ 9_Nano_Projects/0_project_zero/Project_0_E_project/Project_0_E_project.ino
@@ -0,0 +1,31 @@
+
+
+
+#include "Project_0_header.h"
+
+
+ int main (void) 
+  { unsigned int PRN;
+
+  setup_HW;
+LED_1_on;
+
+while(1){
+ 
+   while(switch_1_up);
+
+    if ((Led_1_is_on) && !(Led_2_is_on)){LED_1_off; LED_2_on;}
+     else if (!(Led_1_is_on) && (Led_2_is_on)){LED_1_on;}
+    else if ((Led_1_is_on) && (Led_2_is_on)){LEDs_off;}
+   else if (!(Led_1_is_on) && !(Led_2_is_on)){
+
+PRN = (PRN_8bit_GEN()%3); 
+switch (PRN){
+case 0: LED_1_on; break;
+case 1: LED_2_on; break;
+case 2: LEDs_on; break;}
+}
+Timer_T0_10mS_delay_x_m(20);}}
+
+
+  
