d61330b2158150607596392734efcbebc895b7b8
aclandmark <mark.osborne15@btinternet.com>
2024-08-05 21:59:11 +0100

    Update Project_0_B_project.ino

--- 9_Nano_Projects/0_project_zero/Project_0_B_project/Project_0_B_project.ino
+++ 9_Nano_Projects/0_project_zero/Project_0_B_project/Project_0_B_project.ino
@@ -1,22 +1,24 @@
 /*
-EEPROM usage
-0x1FF, 0x1FE and 0x1FD    OSCCAL
-0x1FC and 0x1FB       PRN generator
+  EEPROM usage
+  0x1FF, 0x1FE and 0x1FD    OSCCAL
+  0x1FC and 0x1FB       PRN generator
 */
 
 
 #include "Project_0_header.h"
 
 
- int main (void)                          //Example 1
-  { 
+int main (void)
+{
 
   setup_HW;
 
-while(1){
+  while (1) {
 
-while (switch_1_up);{switch_LED_1;}
-Timer_T0_10mS_delay_x_m(20);          //Alternative line
-}
-  
+    while (switch_1_up);
+    {
+      switch_LED_1;
     }
+    Timer_T0_10mS_delay_x_m(20);
+  }
+}
