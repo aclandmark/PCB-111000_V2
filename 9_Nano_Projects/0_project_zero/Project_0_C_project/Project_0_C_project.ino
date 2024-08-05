3141172bbe020ae4eb3c73b0507126030aba9eb9
aclandmark <mark.osborne15@btinternet.com>
2024-08-05 21:59:22 +0100

    Update Project_0_C_project.ino

--- 9_Nano_Projects/0_project_zero/Project_0_C_project/Project_0_C_project.ino
+++ 9_Nano_Projects/0_project_zero/Project_0_C_project/Project_0_C_project.ino
@@ -1,23 +1,29 @@
 /*
-EEPROM usage
-0x1FF, 0x1FE and 0x1FD    OSCCAL
-0x1FC and 0x1FB       PRN generator
+  EEPROM usage
+  0x1FF, 0x1FE and 0x1FD    OSCCAL
+  0x1FC and 0x1FB       PRN generator
 */
 
 
 #include "Project_0_header.h"
 
 
- int main (void)                          //Example 1
-  { 
+int main (void)                          //Example 1
+{
 
   setup_HW;
 
-while(1){
+  while (1) {
 
-while ((switch_1_up) && (switch_2_up));
-if(switch_1_up){switch_LED_1;while(switch_2_down);}
-else{switch_LED_2;while(switch_1_down);}
-}
-  
+    while ((switch_1_up) && (switch_2_up));
+    if (switch_1_up)
+    { switch_LED_1;
+      while (switch_2_down);
     }
+    else
+    { switch_LED_2;
+      while (switch_1_down);
+    }
+  }
+
+}
