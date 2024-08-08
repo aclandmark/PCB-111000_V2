e236af43d93c9ea4f354e584abe5878ab060b415
aclandmark <mark.osborne15@btinternet.com>
2024-08-08 16:11:57 +0100

    Update Project_0_A_project.ino

--- 9_Nano_Projects/0_project_zero/Project_0_A_project/Project_0_A_project.ino
+++ 9_Nano_Projects/0_project_zero/Project_0_A_project/Project_0_A_project.ino
@@ -1,7 +1,7 @@
 /*
   EEPROM usage
-  0x1FF, 0x1FE and 0x1FD		OSCCAL
-  0x1FC and 0x1FB				PRN generator
+  0x1FF, 0x1FE and 0x1FD    OSCCAL
+  0x1FC and 0x1FB       PRN generator
 */
 
 
@@ -12,19 +12,8 @@ int main (void)
 {
 
   setup_HW;
+  LED_1_on;
 
-  Timer_T0_10mS_delay_x_m(10);
-  if ((switch_1_down) ) {
-    LED_1_on;
-  }
-  else if ((switch_2_down) ) {
-    LED_2_on;
-  }
-  else if ((switch_3_down) ) {
-    LEDs_on;
-  }
-
-  Timer_T0_10mS_delay_x_m(10);
-
+  
   SW_reset;
 }
