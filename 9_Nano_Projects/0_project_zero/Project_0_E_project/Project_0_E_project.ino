2f949ec26c365c4364f815f1f54c2ca42ea0e33e
aclandmark <mark.osborne15@btinternet.com>
2024-08-08 17:07:29 +0100

    Update Project_0_E_project.ino

--- 9_Nano_Projects/0_project_zero/Project_0_E_project/Project_0_E_project.ino
+++ 9_Nano_Projects/0_project_zero/Project_0_E_project/Project_0_E_project.ino
@@ -4,15 +4,20 @@
   0x1FC and 0x1FB       PRN generator
 */
 
+/*
+ Press sw2 then sw1 to increase rate
+ Hold sw3 and pres asw1 to reduce it 
+ */
+
 
 #include "Project_0_header.h"
 
 
 int main (void)  
 {
-  int on_time = 20;
+  int on_time = 32;
   char switch_released;
-  setup_HW;
+  setup_HW_Basic;
 
   while (1) {
     if (switch_2_up)switch_released = 0;
@@ -21,8 +26,8 @@ int main (void)
     }
     Timer_T0_10mS_delay_x_m(on_time);
 
-    if ((on_time > 2) && (switch_2_down) && (switch_released == 0)) {
-      on_time -= 2;
+    if ((on_time >= 1) && (switch_2_down) && (switch_released == 0)) {
+      on_time /= 2;
       switch_released = 1;
     }
     if (switch_3_down) on_time += 2;
