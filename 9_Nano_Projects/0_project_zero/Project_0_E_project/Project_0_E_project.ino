58b1e7b0f6b89895f8d22cc652edcdc6ff775538
aclandmark <mark.osborne15@btinternet.com>
2024-08-08 16:14:12 +0100

    Update Project_0_E_project.ino

--- 9_Nano_Projects/0_project_zero/Project_0_E_project/Project_0_E_project.ino
+++ 9_Nano_Projects/0_project_zero/Project_0_E_project/Project_0_E_project.ino
@@ -1,38 +1,31 @@
-
+/*
+  EEPROM usage
+  0x1FF, 0x1FE and 0x1FD    OSCCAL
+  0x1FC and 0x1FB       PRN generator
+*/
 
 
 #include "Project_0_header.h"
 
 
-int main (void)
-{ unsigned int PRN;
-
+int main (void)  
+{
+  int on_time = 20;
+  char switch_released;
   setup_HW;
-  LED_1_on;
 
   while (1) {
+    if (switch_2_up)switch_released = 0;
+    while (switch_1_up); {
+      switch_LED_1;
+    }
+    Timer_T0_10mS_delay_x_m(on_time);
 
-    while (switch_1_up);
-
-    if ((Led_1_is_on) && !(Led_2_is_on)) {
-      LED_1_off;
-      LED_2_on;
+    if ((on_time > 2) && (switch_2_down) && (switch_released == 0)) {
+      on_time -= 2;
+      switch_released = 1;
     }
-    else if (!(Led_1_is_on) && (Led_2_is_on)) {
-      LED_1_on;
-    }
-    else if ((Led_1_is_on) && (Led_2_is_on)) {
-      LEDs_off;
-    }
-    else if (!(Led_1_is_on) && !(Led_2_is_on)) {
-
-      PRN = (PRN_8bit_GEN() % 3);
-      switch (PRN) {
-        case 0: LED_1_on; break;
-        case 1: LED_2_on; break;
-        case 2: LEDs_on; break;
-      }
-    }
-    Timer_T0_10mS_delay_x_m(20);
+    if (switch_3_down) on_time += 2;
   }
+
 }
