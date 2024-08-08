bf2533a21526b51c78a67a70298c97f27ee38d05
aclandmark <mark.osborne15@btinternet.com>
2024-08-08 19:36:26 +0100

    Update Project_0_G_project.ino

--- 9_Nano_Projects/0_project_zero/Project_0_G_project/Project_0_G_project.ino
+++ 9_Nano_Projects/0_project_zero/Project_0_G_project/Project_0_G_project.ino
@@ -1,30 +1,61 @@
-/*
-  EEPROM usage
-  0x1FF, 0x1FE and 0x1FD		OSCCAL
-  0x1FC and 0x1FB				PRN generator
-*/
+
 
 
 #include "Project_0_header.h"
 
 
 int main (void)
-{
+{ unsigned int PRN = 1;
 
   setup_HW_Basic;
 
-  Timer_T0_10mS_delay_x_m(10);
-  if ((switch_1_down) ) {
-    LED_1_on;
-  }
-  else if ((switch_2_down) ) {
-    LED_2_on;
-  }
-  else if ((switch_3_down) ) {
-    LEDs_on;
-  }
+while(1){
+    LEDs_off;
+PRN = (PRN_8bit_GEN() % 4);
+      switch (PRN) {
+        case 0: LED_1_on; break;
+        case 1: LED_2_on; break;
+        case 2: LEDs_on; break;
+        case 4:  LEDs_off; break;
+      }
+Timer_T0_10mS_delay_x_m(20);
+while (switch_1_down);
 
-  Timer_T0_10mS_delay_x_m(10);
+}}
 
-  SW_reset;
+
+/*
+  while (1) {
+
+    while (switch_1_up);
+
+    if ((Led_1_is_on) && !(Led_2_is_on)) {
+      LED_1_off;
+      LED_2_on;
+      }
+    else if (!(Led_1_is_on) && (Led_2_is_on)) {
+      LED_1_on;
+    }
+    else if ((Led_1_is_on) && (Led_2_is_on)) {
+      LEDs_off;
+    }
+    else if (!(Led_1_is_on) && !(Led_2_is_on)) {
+
+      PRN = (PRN_8bit_GEN() % 3);
+      switch (PRN) {
+        case 0: LED_1_on; break;
+        case 1: LED_2_on; break;
+        case 2: LEDs_on; break;
+      }
+    }
+    Timer_T0_10mS_delay_x_m(20);
+  }
+}void setup() {
+  // put your setup code here, to run once:
+
 }
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}*/
