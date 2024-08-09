ab433e0aaa4b5e838b6a631bcf70382517d73985
aclandmark <mark.osborne15@btinternet.com>
2024-08-09 11:18:01 +0100

    Update Project_0_G_project.ino

--- 9_Nano_Projects/0_project_zero/Project_0_G_project/Project_0_G_project.ino
+++ 9_Nano_Projects/0_project_zero/Project_0_G_project/Project_0_G_project.ino
@@ -23,39 +23,3 @@ while (switch_1_down);
 
 }}
 
-
-/*
-  while (1) {
-
-    while (switch_1_up);
-
-    if ((Led_1_is_on) && !(Led_2_is_on)) {
-      LED_1_off;
-      LED_2_on;
-      }
-    else if (!(Led_1_is_on) && (Led_2_is_on)) {
-      LED_1_on;
-    }
-    else if ((Led_1_is_on) && (Led_2_is_on)) {
-      LEDs_off;
-    }
-    else if (!(Led_1_is_on) && !(Led_2_is_on)) {
-
-      PRN = (PRN_8bit_GEN() % 3);
-      switch (PRN) {
-        case 0: LED_1_on; break;
-        case 1: LED_2_on; break;
-        case 2: LEDs_on; break;
-      }
-    }
-    Timer_T0_10mS_delay_x_m(20);
-  }
-}void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}*/
