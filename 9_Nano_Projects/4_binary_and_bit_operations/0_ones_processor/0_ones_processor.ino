7e1adf4bb1816dd41adfca85e8dd282ea7d99346
aclandmark <mark.osborne15@btinternet.com>
2024-09-02 21:24:17 +0100

    Update 0_ones_processor.ino

--- 9_Nano_Projects/4_binary_and_bit_operations/0_ones_processor/0_ones_processor.ino
+++ 9_Nano_Projects/4_binary_and_bit_operations/0_ones_processor/0_ones_processor.ino
@@ -1,5 +1,7 @@
 
 #include "0_ones_processor_header.h"
+#include "0_ones_processor_header_2.h"
+
 #define zero 0
 #define one 1
 #define two 2
@@ -9,25 +11,88 @@
 #define six 6
 #define seven 7
 
-#define cpu_reg   digits[2]
+
 
 
 
 char reg_bkp[8];
 char update_reg;
+char bit_num, keypress;
+char digits[8];
 
 int main(void){
-  unsigned int PORT_1 = 1;
-char bit_num, bit_name, keypress;
- char digits[8];
+  char mode;
 
 setup_HW;
-for(int m = 0; m <=7; m++)digits[m] = 0;
-for(int m = 0; m <=7; m++)reg_bkp[m] = 0;
-update_reg = 0;
+
+String_to_PC_Basic("\r\n\r\nManipulating one of the PORT registers\r\n");
+
+String_to_PC_Basic\
+("\r\nStep 1  Set up CPU register\r\n\
+Type bit names\r\n\
+z, o, tw, th, fo, fi, si or se");
+
+do{
+if(select_bits())
+{cpu_reg_1 |= (1 << bit_num);
+Display_registers;} 
+}while (keypress != 'x'); 
+
+String_to_PC_Basic("\r\n\r\nStep 2  Copy to PORT register\r\n");
+waitforkeypress_Basic();
+port_reg = cpu_reg_1;
+cpu_reg_1 = 0;
+Display_registers;
+
+
+String_to_PC_Basic("\r\nTo manipulate bits\r\n\t\
+send 'r' to clear them\r\n\t\
+'s' to set them,\r\n\t\
+'c' to change them and\r\n\t\
+'t' to test them");
 
 while(1){
-if ((keypress = waitforkeypress_Basic()) == 'x')break;
+
+mode = waitforkeypress_Basic();
+
+String_to_PC_Basic("\r\n\r\nStep 1: Set up CPU register");
+
+select_bits_to_process();
+
+switch(mode){
+case 'r': clear_bits; break;
+case 's': set_bits; break;
+case 'c': break;
+case 't': break;}
+
+}
+while(1);
+}
+
+
+
+
+void select_bits_to_process(void){
+do{
+if(select_bits())
+{cpu_reg_1 |= (1 << bit_num);
+Display_registers;}     
+}while (keypress != 'x');}
+
+
+
+
+
+
+char set_update(char bit_num)
+{if(!(reg_bkp[bit_num])){update_reg = 0;reg_bkp[bit_num] = 1;} 
+else update_reg = 1;return update_reg;}
+
+
+
+char select_bits(void){
+    update_reg = 0;
+keypress = waitforkeypress_Basic();
 switch(keypress){
   case 'z': bit_num = zero; set_update(0); break;
   case 'o': bit_num = one;  set_update(1); break;
@@ -42,32 +107,10 @@ switch(keypress){
   case 's': switch(waitforkeypress_Basic()){
     case 'i': bit_num = six; set_update(6); break;
     case 'e': bit_num = seven; set_update(7);break;
-    default: update_reg = 1; break;} break;
-    default: update_reg = 1; break;
-}
-
-if(!(update_reg))
-{cpu_reg |= (1 << bit_num);
-I2C_Tx_BWops(digits);}
-update_reg = 0;}
-
-String_to_PC_Basic("Copy to register");
-waitforkeypress_Basic();
-digits[0] = cpu_reg;
-cpu_reg = 0;
-I2C_Tx_BWops(digits);
-
-while(1);
-}
-
-
-char set_update(char bit_num)
-{if(!(reg_bkp[bit_num])){update_reg = 0;reg_bkp[bit_num] = 1;} 
-else update_reg = 1;return update_reg;}
-
-
-
-
+   default: update_reg = 1; break;} break;
+ case 'x': for(int m = 0; m <=7; m++)reg_bkp[m] = 0; 
+ default: update_reg = 1; break;}
+    if(!(update_reg))return 1; else return 0; }
 
 
 
