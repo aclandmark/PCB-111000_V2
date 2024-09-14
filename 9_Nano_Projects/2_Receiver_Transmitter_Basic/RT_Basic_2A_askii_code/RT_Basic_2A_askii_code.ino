c70b45fd7c8c8a97c032c61fdb2afb3c072a75e8
aclandmark <mark.osborne15@btinternet.com>
2024-09-14 12:44:57 +0100

    Update RT_Basic_2A_askii_code.ino

--- 9_Nano_Projects/2_Receiver_Transmitter_Basic/RT_Basic_2A_askii_code/RT_Basic_2A_askii_code.ino
+++ 9_Nano_Projects/2_Receiver_Transmitter_Basic/RT_Basic_2A_askii_code/RT_Basic_2A_askii_code.ino
@@ -139,9 +139,50 @@ int main (void)                          //Example 5
   
 
 
+*****************Example 6: Sending numbers to the PC*********************************************************
+ int main (void)                          //Example 6
+  { int i = 0, number = 12345;
+  char s[12];
+  setup_HW;
+  wdt_enable(WDTO_120MS);
+  while(switch_1_down)wdr();
+  
+  do {
+    s[i++] = number % 10 + '0';
+  }
+  while ((number = number / 10) > 0);
+  s[i] = '\0';
+  for (int m = i; m > 0; m--)
+    Char_to_PC_Basic(s[m - 1]);
+ 
+ newline_Basic();
+ while(switch_1_up)wdr();
+  while(switch_1_down);
+  return 1;
+  }
 
 
 
+**************Example 7: Entering numbers at the keyboard*****************************************************
+int main (void)                          //Example 7
+  { long num = 0;
+  char keypress;
+  setup_HW;
+  num = 0; Char_to_PC_Basic('?');
+  while ((keypress = waitforkeypress_Basic()) != '\r')
+  { num = num * 10 + keypress  - '0';
+    I2C_Tx_long(num);
+  }
+  Num_to_PC_Local(num * 2);
+    I2C_Tx_long(num * 2);
+    while(switch_1_up);
+    newline_Basic();
+  SW_reset;
+  return 1;
+  }  
+
+
+  
 
 ************This area is for project subroutines*************************************************************/
 void Char_to_PC_Local(char data)
