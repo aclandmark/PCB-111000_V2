f5a07bedbe2f87f0e25140a92dc55f4b677ae4d4
aclandmark <mark.osborne15@btinternet.com>
2024-09-14 13:26:59 +0100

    Update RT_Basic_2A_askii_code.ino

--- 9_Nano_Projects/2_Receiver_Transmitter_Basic/RT_Basic_2A_askii_code/RT_Basic_2A_askii_code.ino
+++ 9_Nano_Projects/2_Receiver_Transmitter_Basic/RT_Basic_2A_askii_code/RT_Basic_2A_askii_code.ino
@@ -19,8 +19,10 @@ digits 1, 2 and 5 can therfore by extracted by subtracting 48 from 49, 50 and 53
 
 #include "Receiver_Transmitter_header.h"
 
- int main (void)                          //Example 1
+  int main (void)                          //Example 1
   { setup_HW;
+  User_prompt;
+  String_to_PC_Basic("\r\nExploring the operation oof the USART\r\n");
   Char_to_PC_Basic('?');
   newline_Basic();
   while (1)
@@ -29,7 +31,6 @@ digits 1, 2 and 5 can therfore by extracted by subtracting 48 from 49, 50 and 53
   }
   return 1;
   }
-
   
 
 
@@ -43,6 +44,8 @@ digits 1, 2 and 5 can therfore by extracted by subtracting 48 from 49, 50 and 53
 ********Example 1: Echoes keypresses*************************************************************************
   int main (void)                          //Example 1
   { setup_HW;
+  User_prompt;
+  String_to_PC_Basic("\r\nExploring the operation oof the USART\r\n");
   Char_to_PC_Basic('?');
   newline_Basic();
   while (1)
