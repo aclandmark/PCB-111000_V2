4d01e98c21a0d153c3594c78eb32d64067b84597
aclandmark <mark.osborne15@btinternet.com>
2024-09-14 12:58:57 +0100

    Create RT_Basic_2B_num_strings.ino

---
+++ 9_Nano_Projects/2_Receiver_Transmitter_Basic/RT_Basic_2B_num_strings/RT_Basic_2B_num_strings.ino
@@ -0,0 +1,183 @@
+
+
+/*
+In this project the "setup_328_HW_Arduino_IO" code segment has been replaced by "setup_328_HW_Basic_IO".
+
+Arduino provides a comprehensive library for sending data to and from a PC.
+However simple DIY (do it yourself) functions are used here to illustate
+the steps involved and introduce the Atmega328 data sheet.
+ 
+ASKII coding is introduced.  Every character on the PC keyboard is assigned a number from 32 to 127.
+For example 63 is used to send a question mark '?' to the PC.
+Decimal 125 is sent as 3 eight bit numbers 49, 50 and 53. 
+48 is used to represent zero.
+digits 1, 2 and 5 can therfore by extracted by subtracting 48 from 49, 50 and 53.  
+5 is then added to 2 times 10 and 1 times 100 to obtain 125 in binary form. 
+*/
+
+
+
+#include "Receiver_Transmitter_header.h"
+
+ int main (void)                          //Example 1
+  { setup_HW;
+  Char_to_PC_Basic('?');
+  newline_Basic();
+  while (1)
+  { Char_to_PC_Local
+    (waitforkeypress_Basic());
+  }
+  return 1;
+  }
+
+  
+
+
+/************************************************************************************************************
+
+  Use this area for saving the examples when they have been got working and finished with
+  Use the space above for the active program (the one being worked on)
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+*******Example 8: Simple arithmetic Data to & from PC*********************************************************
+int main (void)                          //Example 8
+{ long num = 0;
+  char numLength;
+  char Num_string[12];
+  
+  setup_HW;
+  String_to_PC_Basic("?");
+  Num_string_from_KBD_Basic(Num_string);
+
+  { int m = 0; while (Num_string[m])
+      Char_to_PC_Basic(Num_string[m++]);
+  }
+
+  num = Askii_to_binary(Num_string);
+
+  num = num * 2;
+  String_to_PC_Basic("\r\n");
+
+  numLength = Binary_to_Askii(num, Num_string);
+
+  for (int m = numLength; m > 0; m--)
+    Char_to_PC_Basic(Num_string[m - 1]);
+
+  I2C_Tx_long(num);
+while(switch_1_up);
+    newline_Basic();
+  SW_reset;
+  return 1;
+}
+
+
+
+*************Example 9: Arithmetic: More on pointers********************************************************
+int main (void)                          //Example 9
+  { char keyboard_input;
+  char Num_string[12];
+  long Num, A = 55; long B = 7; long Div; long mod;
+  int no_decimal_places;
+
+    setup_HW;
+  String_to_PC_Basic("?\r\n");
+  
+  for(int m = 0; m <=2; m++)
+  {Num_string_from_KBD_Basic(Num_string);
+  Num = Askii_to_binary(Num_string);
+  switch (m)
+  { case 0: A = Num; Char_to_PC_Basic('?');break;
+  case 1: B = Num; String_to_PC_Basic("?\r\n");break;
+  case 2: no_decimal_places = Num; break;}}
+
+  divide(A, B, &Div, &mod, no_decimal_places);
+  Num_to_PC_Local(Div);
+  String_to_PC_Basic(". ");
+    while (no_decimal_places) {
+  no_decimal_places =    divide(mod*10, B, &Div, &mod, no_decimal_places);
+  Num_to_PC_Local(Div);}
+  String_to_PC_Basic("\r\n");
+   SW_reset;
+  return 1; }
+
+  long divide(long A, long B, long *Div, long *mod, int no_decimal_places)
+  {*Div = A/B;
+  *mod = A%B;
+  no_decimal_places -= 1;
+  return no_decimal_places; } 
+
+
+
+************This area is for project subroutines*************************************************************/
+void Num_string_from_KBD_Basic(char * array_ptr)
+{ char keypress;
+  while ((keypress = waitforkeypress_Basic()) != '\r')
+  { *array_ptr = keypress;
+    array_ptr += 1;
+  }
+  *array_ptr = '\0';
+}
+
+
+
+/********************************************************************************************************/
+long Askii_to_binary(char * array_ptr) {
+  long num = 0;
+{ int m = 0; while (*(array_ptr + m)) {
+      num = num * 10 + (*(array_ptr + m++))  - '0';
+    }
+  }
+  return num;
+}
+
+
+
+/********************************************************************************************************/
+int Binary_to_Askii (long number, char * array_ptr)
+{ int i = 0;
+  do {
+    array_ptr[i++] = number % 10 + '0';
+  }
+  while ((number = number / 10) > 0);
+  array_ptr[i] = '\0';
+  return i;
+}
+
+
+/********************************************************************************************************/
+void Char_to_PC_Local(char data)
+{ while (!(UCSR0A & (1 << UDRE0)));
+  UDR0 = data;
+}
+
+
+
+
+/********************************************************************************************************/
+void Num_to_PC_Local (long number)
+{ int i = 0;
+  char s[12];
+  
+  do
+  { s[i++] = number % 10 + '0';
+  }
+  while ((number = number / 10) > 0);
+  s[i] = '\0';
+  for (int m = i; m > 0; m--)Char_to_PC_Basic(s[m - 1]);
+  Char_to_PC_Basic(' ');
+}
+
+
+
+/********************************************************************************************************/
