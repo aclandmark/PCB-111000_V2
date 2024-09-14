7475f4f2186aa9b0c70a24dcfa76c1d43a39bb6f
aclandmark <mark.osborne15@btinternet.com>
2024-09-14 13:22:52 +0100

    Update RT_Basic_2B_num_strings.ino

--- 9_Nano_Projects/2_Receiver_Transmitter_Basic/RT_Basic_2B_num_strings/RT_Basic_2B_num_strings.ino
+++ 9_Nano_Projects/2_Receiver_Transmitter_Basic/RT_Basic_2B_num_strings/RT_Basic_2B_num_strings.ino
@@ -19,103 +19,64 @@ digits 1, 2 and 5 can therfore by extracted by subtracting 48 from 49, 50 and 53
 
 #include "Receiver_Transmitter_header.h"
 
- int main (void)                          //Example 1
-  { setup_HW;
-  Char_to_PC_Basic('?');
-  newline_Basic();
-  while (1)
-  { Char_to_PC_Local
-    (waitforkeypress_Basic());
-  }
-  return 1;
-  }
-
-  
-
-
-/************************************************************************************************************
-
-  Use this area for saving the examples when they have been got working and finished with
-  Use the space above for the active program (the one being worked on)
-
-
-
-
+ 
  
 
 
-
-
-
-
-
-
-*******Example 8: Simple arithmetic Data to & from PC*********************************************************
-int main (void)                          //Example 8
-{ long num = 0;
-  char numLength;
-  char Num_string[12];
-  
+*****************Example 6: Sending numbers to the PC*********************************************************
+ int main (void)                          //Example 6
+  { int i = 0, number = 12345;
+  char s[12];
   setup_HW;
-  String_to_PC_Basic("?");
-  Num_string_from_KBD_Basic(Num_string);
-
-  { int m = 0; while (Num_string[m])
-      Char_to_PC_Basic(Num_string[m++]);
+  wdt_enable(WDTO_120MS);
+  while(switch_1_down)wdr();
+  
+  do {
+    s[i++] = number % 10 + '0';
+  }
+  while ((number = number / 10) > 0);
+  s[i] = '\0';
+  for (int m = i; m > 0; m--)
+    Char_to_PC_Basic(s[m - 1]);
+ 
+ newline_Basic();
+ while(switch_1_up)wdr();
+  while(switch_1_down);
+  return 1;
   }
 
-  num = Askii_to_binary(Num_string);
 
-  num = num * 2;
-  String_to_PC_Basic("\r\n");
 
-  numLength = Binary_to_Askii(num, Num_string);
-
-  for (int m = numLength; m > 0; m--)
-    Char_to_PC_Basic(Num_string[m - 1]);
-
-  I2C_Tx_long(num);
-while(switch_1_up);
+**************Example 7: Entering numbers at the keyboard*****************************************************
+int main (void)                          //Example 7
+  { long num = 0;
+  char keypress;
+  setup_HW;
+  num = 0; Char_to_PC_Basic('?');
+  while ((keypress = waitforkeypress_Basic()) != '\r')
+  { num = num * 10 + keypress  - '0';
+    I2C_Tx_long(num);
+  }
+  Num_to_PC_Local(num * 2);
+    I2C_Tx_long(num * 2);
+    while(switch_1_up);
     newline_Basic();
   SW_reset;
   return 1;
-}
+  }  
 
 
 
-*************Example 9: Arithmetic: More on pointers********************************************************
-int main (void)                          //Example 9
-  { char keyboard_input;
-  char Num_string[12];
-  long Num, A = 55; long B = 7; long Div; long mod;
-  int no_decimal_places;
 
-    setup_HW;
-  String_to_PC_Basic("?\r\n");
-  
-  for(int m = 0; m <=2; m++)
-  {Num_string_from_KBD_Basic(Num_string);
-  Num = Askii_to_binary(Num_string);
-  switch (m)
-  { case 0: A = Num; Char_to_PC_Basic('?');break;
-  case 1: B = Num; String_to_PC_Basic("?\r\n");break;
-  case 2: no_decimal_places = Num; break;}}
 
-  divide(A, B, &Div, &mod, no_decimal_places);
-  Num_to_PC_Local(Div);
-  String_to_PC_Basic(". ");
-    while (no_decimal_places) {
-  no_decimal_places =    divide(mod*10, B, &Div, &mod, no_decimal_places);
-  Num_to_PC_Local(Div);}
-  String_to_PC_Basic("\r\n");
-   SW_reset;
-  return 1; }
 
-  long divide(long A, long B, long *Div, long *mod, int no_decimal_places)
-  {*Div = A/B;
-  *mod = A%B;
-  no_decimal_places -= 1;
-  return no_decimal_places; } 
+
+
+
+
+
+
+
 
 
 
