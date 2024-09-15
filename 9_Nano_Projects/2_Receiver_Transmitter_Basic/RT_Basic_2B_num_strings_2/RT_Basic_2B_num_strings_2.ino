f62232580d27b9645717bb1b8b08200ffbb7a483
aclandmark <mark.osborne15@btinternet.com>
2024-09-15 21:04:18 +0100

    Update RT_Basic_2B_num_strings_2.ino

--- 9_Nano_Projects/2_Receiver_Transmitter_Basic/RT_Basic_2B_num_strings_2/RT_Basic_2B_num_strings_2.ino
+++ 9_Nano_Projects/2_Receiver_Transmitter_Basic/RT_Basic_2B_num_strings_2/RT_Basic_2B_num_strings_2.ino
@@ -1,41 +1,81 @@
 
 
+#include "Receiver_Transmitter_header.h"
 
-long Int_KBD_to_display_A_Local(char display_buffer[]){                //Acquires an integer string from the keyboard and returns the binary equivalent
+
+int main (void){
+  char num_string[12];
+setup_HW;
+User_prompt;
+Num_string_from_KBD_Basic(num_string);
+newline_Basic();
+String_to_PC_Basic(num_string);
+SW_reset;
+return 1;}
+
+
+
+void Num_string_from_KBD_Basic(char * array_ptr)
+{ char keypress;
+  while ((keypress = waitforkeypress_Basic()) != '\r')
+  { *array_ptr = keypress;
+    Char_to_PC_Basic(keypress);
+    array_ptr += 1;
+  }
+  *array_ptr = '\0';}
+
+
+
+/********************************************************************************************************/
+long Askii_to_binary(char * array_ptr) {
+  long num = 0;
+{ int m = 0; while (*(array_ptr + m)) {
+      num = num * 10 + (*(array_ptr + m++))  - '0';
+    }
+  }
+  return num;}
+
+
+/*************************************************************************************************************/
+ void num_to_PC (long number)                          //Example 6
+  { 
+  
+  while(1) {
+    s[i++] = number % 10 + '0';
+    number = number/10;
+    if (!(number))break;
+  }s[i] = '\0';
+  
+    for (int m = i; m > 0; m--)
+    Char_to_PC_Basic(s[m - 1]);
+ 
+ newline_Basic();
+  return 1;
+  }
+  
+/*long Int_KBD_to_display_A_Local(char num_string[]){                //Acquires an integer string from the keyboard and returns the binary equivalent
 char keypress;
 long Long_Num_from_mini_OS;
-
-for(int n = 0; n<=8; n++) display_buffer[n] = 0;              
+int s=0;
 
 while(1){
-keypress = waitforkeypress_A();
-if ((!(decimal_digit_A(keypress)))
+keypress = waitforkeypress_Basic();
+if ((!(decimal_digit_Basic(keypress)))
 && (keypress != '-'))continue;                                //Ignore keypress if it is not OK
-display_buffer[0] = keypress;
+Char_to_PC_Basic(keypress);
+num_string[s++] = keypress;
 break;}
  
-I2C_Tx_8_byte_array(display_buffer);                    //See "Chip2chip_comms\Display_driver_header.h"
 while(1){
-if ((keypress = wait_for_return_key_A())  =='\r')break;         //Detect return key press (i.e \r or\r\n)
-if ((decimal_digit_A(keypress)) || (keypress == '\b')\
- || (keypress == '-'))
+if ((keypress = wait_for_return_key_Basic())  =='\r')break;         //Detect return key press (i.e \r or\r\n)
+if ((decimal_digit_Basic(keypress)) || (keypress == '-'))
+{num_string[s++] = keypress;
+Char_to_PC_Basic(keypress);
+}}  
+newline_Basic();                                             
+String_to_PC_Basic(num_string);
+return 1;}*/
 
-{if (keypress == '\b'){                                 //Backspace key
-for (int n = 0; n < 7; n++)
-display_buffer[n] = display_buffer[n + 1];
-display_buffer[7] = 0;}
 
-else
-{for(int n = 8; n>=1; n--)                                      //Shift display for each new keypress
-display_buffer[n] = display_buffer[n-1];
-display_buffer[0] = keypress;  }                              //Add new keypress           
 
-I2C_Tx_8_byte_array(display_buffer);}}                                     //Update display includes "cr_keypress"                                                 
-
-I2C_Tx_any_segment_clear_all();
-wdr();_delay_ms(50);wdr();_delay_ms(50);
-I2C_Tx_8_byte_array(display_buffer);
-
-Long_Num_from_mini_OS = I2C_displayToNum();
-
-return Long_Num_from_mini_OS;}
+/********************************************************************************************************************/
