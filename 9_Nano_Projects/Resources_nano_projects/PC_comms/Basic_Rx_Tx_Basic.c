9028b80ffc5f99476868649ba959a2ef5d7904a5
aclandmark <mark.osborne15@btinternet.com>
2024-09-17 13:15:59 +0100

    Update Basic_Rx_Tx_Basic.c

--- 9_Nano_Projects/Resources_nano_projects/PC_comms/Basic_Rx_Tx_Basic.c
+++ 9_Nano_Projects/Resources_nano_projects/PC_comms/Basic_Rx_Tx_Basic.c
@@ -111,6 +111,27 @@ I2C_Tx_8_byte_array(digits);}}
 return I2C_displayToNum();}
 
 
+/***********************************************************************************************************************************************************************************/
+void Hex_and_Int_to_PC_Basic (char radix, int number)
+{ int i = 0;
+ char sign = '+';
+  unsigned char s[12];
+unsigned int num;
+  
+if ((number < 0) && (radix == 10)){sign = '-'; number *= -1;}
+    num = (unsigned)number;  
+   
+   do  { s[i] = num % radix;// + '0';
+if (s[i] < 10)s[i] += '0'; else s[i] += '7';
+   i += 1;  }
+  while ((num = num / radix) > 0);
+  s[i] = '\0';
+  if ((sign == '-') && (radix == 10)){Char_to_PC_Basic('-');}
+  for (int m = i; m > 0; m--)Char_to_PC_Basic(s[m - 1]);
+  Char_to_PC_Basic(' ');}
+
+
+
 
 /**********************************************************************************************************************************************************************************/
 char wait_for_return_key_Basic(void){                  						//Returns key presses one at a time
