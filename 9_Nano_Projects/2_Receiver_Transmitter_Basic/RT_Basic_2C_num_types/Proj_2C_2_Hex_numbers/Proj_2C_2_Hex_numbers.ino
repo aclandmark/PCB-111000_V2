35ef6b9ea56582696df49a36892e31ebe74705b7
aclandmark <mark.osborne15@btinternet.com>
2024-09-17 13:16:13 +0100

    Update Proj_2C_2_Hex_numbers.ino

--- 9_Nano_Projects/2_Receiver_Transmitter_Basic/RT_Basic_2C_num_types/Proj_2C_2_Hex_numbers/Proj_2C_2_Hex_numbers.ino
+++ 9_Nano_Projects/2_Receiver_Transmitter_Basic/RT_Basic_2C_num_types/Proj_2C_2_Hex_numbers/Proj_2C_2_Hex_numbers.ino
@@ -27,40 +27,27 @@ setup_HW;
 
   String_to_PC_Basic("Enter positive number and increment then AK or x\r\n");  
     Num = Int_from_PC_Basic(digits);
-    Int_to_PC_local(10, Num);
+    Hex_and_Int_to_PC_Basic(10, Num);
     Inc = Int_from_PC_Basic(digits);
   
-  Int_to_PC_local(10, Inc);
+  Hex_and_Int_to_PC_Basic(10, Inc);
   newline_Basic();
     
-do{ 
-   Int_to_PC_local(10, Num);
+while(1){ 
+   Hex_and_Int_to_PC_Basic(10, Num);
   Char_to_PC_Basic('\t');
- Int_to_PC_local(radix, Num);Char_to_PC_Basic('\t');     //Send the number to the pc with a radix of 16.
+ Hex_and_Int_to_PC_Basic(radix, Num);Char_to_PC_Basic('\t');     //Send the number to the pc with a radix of 16.
   Binary_to_PC_local(Num);              //Send the number to the pc in binary form
   String_to_PC_Basic("\r\n");
-  Num= Num + Inc;} while(waitforkeypress_Basic() != 'x'); //increment the number
-  SW_reset;}
+  
+  switch(waitforkeypress_Basic()){
+   case 'x': Num= Num + Inc;break;
+   case 'y': Num= Num - Inc;break;
+   default:SW_reset;break;    
+  }}
+  }
   
   
-
-/**************************************************************************************************/
-/*void NumericString_to_PC_local(unsigned char* s){
-int n=0;
-while (s[n] != '\0')n++;                //scan through the string until the '\0' is found
-for(int m = n; m; m--)Char_to_PC_Basic(*(s + m-1));     //send characters to the pc finishing with s[0]
-Timer_T0_10mS_delay_x_m(4);}              //rather than starting with it
-*/
-
-/**************************************************************************************************/
-/*void Local_BtoA(long num,unsigned char digit_array[]){     //Local function only: check with paper and pencil
-int m=0;
-do  {digit_array[m++] = (num%10) + '0';}
-while((num = num/10) > 0);
-digit_array[m] = 0;}
-
-
-
 /**************************************************************************************************/
 void Binary_to_PC_local(int x){             //check with paper and pencil
 for (int m = 0; m<16; m++){
@@ -69,29 +56,7 @@ for (int m = 0; m<16; m++){
   if(m%4==3)String_to_PC_Basic("  ");}}
 
 /*******************************************************************************************************/
-void Int_to_PC_local (char radix, int number)
-{ int i = 0;
- char sign = '+';
-  unsigned char s[12];
-unsigned int num;
-  
-if ((number < 0) && (radix == 10)){sign = '-'; number *= -1;}
- //if ((number < 0) && (radix == 16)){number *= -1; sign = '-';}
-  
-   num = (unsigned)number;  
-   do
-  { s[i] = num % radix;// + '0';
-if (s[i] < 10)s[i] += '0'; else s[i] += '7';
-   i += 1; 
-  }
-  while ((num = num / radix) > 0);
-  s[i] = '\0';
-  
-if ((sign == '-') && (radix == 10)){Char_to_PC_Basic('-');}
- //if ((sign == '-') && (radix == 16))number *= -1; 
 
-//if (sign == '-')Char_to_PC_Basic('-');
-  
-  for (int m = i; m > 0; m--)Char_to_PC_Basic(s[m - 1]);
-  Char_to_PC_Basic(' ');
-}
+
+
+  /***************************************************************************************/
