caee5540ccdfb8aaddf7fdd5f2f6ef0520798fbe
aclandmark <mark.osborne15@btinternet.com>
2024-09-18 17:41:55 +0100

    Update Integer_arithmetic.ino

--- 9_Nano_Projects/2_Receiver_Transmitter_Basic/RT_Basic_2D_arithmetic/Integer_arithmetic/Integer_arithmetic.ino
+++ 9_Nano_Projects/2_Receiver_Transmitter_Basic/RT_Basic_2D_arithmetic/Integer_arithmetic/Integer_arithmetic.ino
@@ -4,26 +4,28 @@
 /*******Example 8: Simple arithmetic Data to & from PC*********************************************************/
 int main (void)                          //Example 8
 { long num_1, num_2;
-  char numLength;
-  char Num_string[12];
+ // char numLength;
+ char Num_string[12];
   char op;
   
   setup_HW;
-  String_to_PC_Basic("Enter integer and op");
+  String_to_PC_Basic("Enter positive integers and op");
   num_1 = Int_from_PC_Basic(Num_string);
 
 while(1){
 op = waitforkeypress_Basic();
 num_2 = Int_from_PC_Basic(Num_string);
 switch (op){
+  case '+': num_1 = num_1 + num_2;break;
+  case '-': num_1 = num_1 - num_2;break;
   case '*': num_1 = num_1 * num_2;break;
-  
+  case '/': num_1 = num_1 / num_2;break;
   }
 
   
   Char_to_PC_Basic('\t');
 
- Hex_and_Int_to_PC_Basic(10, num_1);
+ Long_Hex_and_Int_to_PC_Basic(10, num_1);
 
   I2C_Tx_long(num_1);
 //while(switch_1_up);
