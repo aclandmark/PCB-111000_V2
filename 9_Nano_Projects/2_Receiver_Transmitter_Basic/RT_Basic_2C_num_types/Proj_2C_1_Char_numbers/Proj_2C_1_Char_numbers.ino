3809db85efe6012cddde7776ca691ccbafbdd930
aclandmark <mark.osborne15@btinternet.com>
2024-09-18 15:12:16 +0100

    Update Proj_2C_1_Char_numbers.ino

--- 9_Nano_Projects/2_Receiver_Transmitter_Basic/RT_Basic_2C_num_types/Proj_2C_1_Char_numbers/Proj_2C_1_Char_numbers.ino
+++ 9_Nano_Projects/2_Receiver_Transmitter_Basic/RT_Basic_2C_num_types/Proj_2C_1_Char_numbers/Proj_2C_1_Char_numbers.ino
@@ -3,16 +3,18 @@
 
 
 #include "Proj_2_C1_header_file.h"
+#include "Local_subroutines.c"
 
 
+extern char digit;
+extern char mode;
+ 
 
 
-char digit=1;
- char op, mode;
 
 int main (void){
-
-op=0;
+char op=0;
+digit=1;
 mode = 'u';                         //mode: signed or unsigned
 setup_HW;
 I2C_Tx_display_char(digit,mode);update_screen();
@@ -35,52 +37,6 @@ Timer_T0_10mS_delay_x_m(15);
 op = 0;}}
 
 
-void Short_Int_to_PC_Basic (char radix, char number, char mode)
-{ int i = 0;
-   char s[12];
-   char sign = '+';
-   unsigned char num;
-   
-    num = (byte)number;
-   if(radix == 10){  if ((mode == 's') && (number < 0)){num = -number; sign = '-';}}
-   do
-  { s[i] = num % radix;// + '0';
-if (s[i] < 10)s[i] += '0'; else s[i] += '7';
-   i += 1; 
-  }
-  while ((num = num / radix) > 0);
-  s[i] = '\0';
-  if (sign == '-')Char_to_PC_Basic('-');
-  for (int m = i; m > 0; m--)Char_to_PC_Basic(s[m - 1]);
-  Char_to_PC_Basic(' ');
-}
-
-
-
-void update_screen(void){
-
-Short_Int_to_PC_Basic (10,digit, mode);
-Char_to_PC_Basic ('\t');  
-Short_Int_to_PC_Basic (16,digit, mode);
-Char_to_PC_Basic ('\t');
-Short_binary_to_PC_local(digit);
-
-if(mode == 's'){
-Char_to_PC_Basic ('\t');
-Short_binary_to_PC_local((~digit) + 1);
-Char_to_PC_Basic ('\t');
-Short_Int_to_PC_Basic (16, (~digit)+1, + mode);
-Char_to_PC_Basic ('\t');
-Short_Int_to_PC_Basic (10, (~digit)+1, + mode);}
-
-newline_Basic();}
-
-
-void Short_binary_to_PC_local(int x){             //check with paper and pencil
-for (int m = 0; m<8; m++){
-  if(x & (1 << (7-m))) Char_to_PC_Basic('1');        //test each bit in turn starting with the most significant
-  else Char_to_PC_Basic('0');   
-  if(m%4==3)String_to_PC_Basic("  ");}}
 
 
 
