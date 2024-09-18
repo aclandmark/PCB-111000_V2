bee992d2668807f18b8954bed1304122b6f8bddc
aclandmark <mark.osborne15@btinternet.com>
2024-09-18 20:52:06 +0100

    Update Real_num_division.ino

--- 9_Nano_Projects/2_Receiver_Transmitter_Basic/RT_Basic_2D_arithmetic/Real_num_division/Real_num_division.ino
+++ 9_Nano_Projects/2_Receiver_Transmitter_Basic/RT_Basic_2D_arithmetic/Real_num_division/Real_num_division.ino
@@ -1,5 +1,8 @@
+#include "Real_num_division.h"
 
-*************Example 9: Arithmetic: More on pointers********************************************************
+
+
+//*************Example 9: Arithmetic: More on pointers********************************************************
 int main (void)                          //Example 9
   { char keyboard_input;
   char Num_string[12];
@@ -10,25 +13,32 @@ int main (void)                          //Example 9
   String_to_PC_Basic("?\r\n");
   
   for(int m = 0; m <=2; m++)
-  {Num_string_from_KBD_Basic(Num_string);
-  Num = Askii_to_binary(Num_string);
+  {//Num_string_from_KBD_Basic(Num_string);
+  Num = Int_from_PC_Basic(Num_string);
   switch (m)
   { case 0: A = Num; Char_to_PC_Basic('?');break;
   case 1: B = Num; String_to_PC_Basic("?\r\n");break;
   case 2: no_decimal_places = Num; break;}}
 
   divide(A, B, &Div, &mod, no_decimal_places);
-  Num_to_PC_Local(Div);
+  Long_Hex_and_Int_to_PC_Basic(10,Div);
   String_to_PC_Basic(". ");
     while (no_decimal_places) {
   no_decimal_places =    divide(mod*10, B, &Div, &mod, no_decimal_places);
-  Num_to_PC_Local(Div);}
+  //Long_Hex_and_Int_to_PC_Basic(10,Div);
+  Char_to_PC_Basic(Div + '0');}
   String_to_PC_Basic("\r\n");
    SW_reset;
   return 1; }
 
+
+
+/***********************************************************************************************/
   long divide(long A, long B, long *Div, long *mod, int no_decimal_places)
   {*Div = A/B;
   *mod = A%B;
   no_decimal_places -= 1;
   return no_decimal_places; } 
+
+
+  /****************************************************************************************************/
