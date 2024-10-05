f8bde785c382449926a7650b995aa53096a491af
aclandmark <mark.osborne15@btinternet.com>
2024-10-05 20:47:58 +0100

    Update 2A_4_Integer_numbers.ino

--- 9_Nano_Projects/2A_Display_Driver_Basic/2A_4_Arithmetic/2A_4_Integer_numbers/2A_4_Integer_numbers.ino
+++ 9_Nano_Projects/2A_Display_Driver_Basic/2A_4_Arithmetic/2A_4_Integer_numbers/2A_4_Integer_numbers.ino
@@ -4,37 +4,29 @@
 #include "Integer_numbers_header.h"
 
 #define message_1 "\r\nEnter integer number:  PCB returns all factors of that number.(Press sw1 to cancel.)\r\n"
-#define message_2 "New number?\r\n"
-#define message_3 "\r\nCalculation cancelled. New number?\r\n"
+#define message_2 "\r\nNew number?\r\n"
 
-
-#define BL 30                     //Buffer length
 
 int main (void)  
   { 
-    char num_string[BL + 2];
+    char num_string[12];
     long  num;
     long m = 1;
  
- setup_HW_Arduino;
- set_up_pci_on_sw1_and_sw3;
- enable_pci_on_sw1_and_sw3
- 
-switch(reset_status){
-  case POR_reset:                 User_prompt_A;    Serial.write(message_1);break;
-  case WDT_reset:                 Serial.write(message_2);break;
-  case WDT_reset_with_flag:       Serial.write(message_3);break;
-  case External_reset:            Serial.write(message_1);break;}
- 
-num = Int_Num_from_PC_A_Local(num_string,BL);
+ setup_HW;
 
-newline_A();
-Serial.print(num);
-Serial.write("\t");
+if(!(watch_dog_reset))String_to_PC_Basic(message_1);
+else String_to_PC_Basic(message_2);
+  
+num = Int_from_PC_Local(num_string);
+
+newline_Basic();
+Int_to_PC_Basic(num);
+String_to_PC_Basic("\t");
 
 do{
-if(!(num%m)){Int_Num_to_PC_A_Local(m, num_string, '\t');}
-m += 1; if(!(m%10000))Serial.write(".");}while(m < num);
+if(!(num%m)){Int_to_PC_Basic(m);}
+m += 1; if(!(m%10000))String_to_PC_Basic(".");}while(m < num);
 
 SW_reset;
 return 1;  }
@@ -44,27 +36,28 @@ return 1;  }
 
 
 /******************************************************************************************/
-long Int_Num_from_PC_A_Local(char * num_as_string, char bufferlen)    //Project version implements the backspace key
-{char strln;
-Serial.flush();   
-strln = Serial.readBytesUntil('\r',num_as_string, bufferlen);
-num_as_string[strln] = 0;
-return atol(num_as_string);}
+long Int_from_PC_Local(char digits[]){
+char keypress;
+for(int n = 0; n<=7; n++) digits[n] = 0; 
+
+do
+{keypress =  waitforkeypress_Basic();} 
+while (!(decimal_digit_Basic(keypress)));                                      //(non_decimal_char(keypress));  //Not -,0,1,2,3,4,5,6,7,8 or 9
+digits[0] = keypress;
+I2C_Tx_8_byte_array(digits);
+
+while(1){
+if ((keypress = wait_for_return_key_Basic())  =='\r')break;
+if (decimal_digit_Basic (keypress))                                           //012345678or9  :Builds up the number one keypress at a time
+{for(int n = 7; n>=1; n--)
+digits[n] = digits[n-1];                                                //Shifts display left for each keypress
+digits[0] = keypress;
+I2C_Tx_8_byte_array(digits);}}
+            
+return I2C_displayToNum();}
 
 
 
-/******************************************************************************************/
-void Int_Num_to_PC_A_Local(long Int_num, char * num_as_string, char next_char)
-{ltoa(Int_num, num_as_string, 10);
-Serial.print(num_as_string);
-Serial.print(next_char);}
-
-
-ISR(PCINT2_vect){
-if(switch_3_down) return;
-if((switch_1_up) && (switch_3_up))return;
- if(switch_1_down) eeprom_write_byte((uint8_t*)0x1FA, 0);
- SW_reset;}
 
 
 /******************************************************************************************/
