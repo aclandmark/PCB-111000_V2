9d95b9f4530786a7a270326576c87c43cc66f234
aclandmark <mark.osborne15@btinternet.com>
2024-11-05 21:41:39 +0000

    Update 2A_4_Num_from_KBD.ino

--- 9_Nano_Projects/2A_Display_Driver_Basic/2A_4_Num_from_KBD/2A_4_Num_from_KBD.ino
+++ 9_Nano_Projects/2A_Display_Driver_Basic/2A_4_Num_from_KBD/2A_4_Num_from_KBD.ino
@@ -1,78 +1,53 @@
-/* Numbers from the keyboard*/
+/* Numbers from the keyboard try 67108864*/
 
 #include "Receiver_Transmitter_header.h"
 
-int main (void){                    //Example 1
+int main (void){   
   char num_string[12];
   long num;
-setup_HW;
-if(watch_dog_reset == 1)watch_dog_reset = 0; else
-String_to_PC_Basic("Enter number at keyboard\r\n");
-Num_string_from_KBD_Local(num_string);
-Char_to_PC_Basic('\t');
-num = Askii_to_binary_Local(num_string);
-Int_to_PC_Basic(num/2);
-newline_Basic();
-SW_reset;
-return 1;}
-
-
-/*
-int main (void){                      //Example 2
-  char num_string[12];
-  long num = 2;
-  long old_num = 2;
-setup_HW;
-String_to_PC_Basic("Multiply number by 2 and enter it\r\n");
-String_to_PC_Basic("2\r\n");
-while(1){
   
-Num_string_from_KBD_Local(num_string);
-num = Askii_to_binary_Local(num_string);
-
-if(num == old_num * 2)
-{newline_Basic();old_num = num;}
-else Char_to_PC_Basic('?');
-}
+setup_HW;
+if(watch_dog_reset == 1)String_to_PC_Basic("\r\nAgain"); else
+String_to_PC_Basic("Enter number at keyboard");
+num = Int_KBD_to_display_A_Local(num_string);
+waitforkeypress_Basic();
+while(num){
+num /= 2;
+I2C_Tx_long(num);
+waitforkeypress_Basic();}
 SW_reset;
 return 1;}
 
-*/
+
+
+
+/************************************************************************/
+long Int_KBD_to_display_A_Local(char display_buffer[]){                //Acquires an integer string from the keyboard and returns the binary equivalent
+char keypress;
+long Long_Num_from_mini_OS;
+
+for(int n = 0; n<=8; n++) display_buffer[n] = 0;              
+
+while(1){
+if ((keypress = wait_for_return_key_Basic())=='\r')break;         //Detect return key press (i.e \r or\r\n)
+if (!(decimal_digit_Basic(keypress)))continue;
+
+if(display_buffer[0]){for(int n = 8; n>=1; n--)                                      //Shift display for each new keypress
+display_buffer[n] = display_buffer[n-1];}
+display_buffer[0] = keypress;                                  //Add new keypress           
+
+I2C_Tx_8_byte_array(display_buffer);}                           //Update display includes "cr_keypress"                                                 
+
+I2C_Tx_any_segment_clear_all();
+wdr();_delay_ms(50);wdr();_delay_ms(50);
+I2C_Tx_8_byte_array(display_buffer);
+
+Long_Num_from_mini_OS = I2C_displayToNum();
+
+return Long_Num_from_mini_OS;}
+
+
+
 
 
 /***********************Subroutines*********************************/
-void Num_string_from_KBD_Local(char * array_ptr)
-{ char keypress;
-  while ((keypress = waitforkeypress_Basic()) != '\r')
-  { *array_ptr = keypress;
-    Char_to_PC_Basic(keypress);
-    array_ptr += 1;
-  }
-  *array_ptr = '\0';}
-
-
-
-/********************************************************************************************************/
-long Askii_to_binary_Local(char * array_ptr) {
-  long num = 0;
-{ int m = 0; while (*(array_ptr + m)) {
-      num = num * 10 + (*(array_ptr + m++))  - '0';
-    }
-  }
-  return num;}
-
-
-
-/*************************************************************************************************************/
-/* void num_to_PC_Local (long number)
-{ int i = 0;
-  char s[12];
-   do
-  { s[i++] = number % 10 + '0';
-  }
-  while ((number = number / 10) > 0);
-  s[i] = '\0';
-  for (int m = i; m > 0; m--)Char_to_PC_Basic(s[m - 1]);
-  Char_to_PC_Basic(' ');}*/
-
-/********************************************************************************************************************/
